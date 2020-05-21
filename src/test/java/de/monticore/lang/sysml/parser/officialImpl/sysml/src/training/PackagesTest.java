a3ba04eee4092cb2a7d7f974d9f4b8f6acd6721a
RobinMuenstermann <robin.muenstermann@rwth-aachen.de>
2020-05-21 15:13:48 +0200

    added structure for testing

---
+++ src/test/java/de/monticore/lang/sysml/parser/officialImpl/sysml/src/training/PackagesTest.java
@@ -0,0 +1,11 @@
+
+
+package de.monticore.lang.sysml.parser.officialImpl.sysml.src.training;
+
+/**
+ * @author Robin Muenstermann
+ * @version 1.0
+ */
+public class PackagesTest {
+
+}
---
+++ src/test/java/de/monticore/lang/sysml/utils/Parser.java
@@ -0,0 +1,42 @@
+
+
+package de.monticore.lang.sysml.utils;
+
+import de.monticore.lang.sysml.legacy.sysml2._ast.ASTSysMLPackage;
+import de.monticore.lang.sysml.legacy.sysml2._parser.SysML2Parser;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO implement me
+ *
+ * @author Robin Muenstermann
+ * @version 1.0
+ */
+public class Parser {
+
+  private String pathToFile;
+  Parser(String pathToFile){
+    this.pathToFile = pathToFile;
+  }
+
+  void parse(){
+    SysML2Parser parser = new SysML2Parser();// TODO
+    Path model = Paths.get(this.pathToFile);
+
+    try {
+      Optional<ASTSysMLPackage> sysmlPackage = parser.parse(model.toString());//TODO
+      assertFalse(parser.hasErrors());
+      assertTrue(sysmlPackage.isPresent());
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      fail("There was an exception when parsing the model " + model + ": " + e.getMessage());
+    }
+  }
+}
