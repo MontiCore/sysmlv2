d481a8749d9e2d88a3fee503c38e9c4baf542eb4
RobinMuenstermann <robin.muenstermann@rwth-aachen.de>
2020-06-12 17:02:29 +0200

    Working on Parsing Package Example, fixed Error with the interface PackagedDefinitionMember, added alias, problem with QualifiedName

--- src/test/java/de/monticore/lang/sysml/parser/officialImpl/sysml/src/training/ParsingTrainingTest.java
+++ src/test/java/de/monticore/lang/sysml/parser/officialImpl/sysml/src/training/ParsingTrainingTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.*;
  */
 public class ParsingTrainingTest {
 
-  @Ignore
   @Test
   public void parseAllSpecifiedTrainingFilesTest(){
     PathsToFile pathsToFile = new PathsToFile();
