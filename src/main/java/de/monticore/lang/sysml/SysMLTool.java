acaacd77f0e7eee1711e33131b9b53934b20de9c
RobinMuenstermann <robin.muenstermann@rwth-aachen.de>
2020-09-28 18:04:04 +0200

    Create SymbolTable.

--- src/main/java/de/monticore/lang/sysml/SysMLTool.java
+++ src/main/java/de/monticore/lang/sysml/SysMLTool.java
@@ -1,5 +1,6 @@
 package de.monticore.lang.sysml;
 
+import com.google.common.collect.Sets;
 import de.monticore.io.paths.ModelPath;
 import de.monticore.lang.sysml._symboltable.HelperSysMLSymbolTableCreator;
 import de.monticore.lang.sysml._symboltable.SysMLLanguageSub;
@@ -9,6 +10,7 @@ import de.monticore.lang.sysml.parser.SysMLParserMultipleFiles;
 import de.monticore.lang.sysml.sysml._symboltable.SysMLArtifactScope;
 import de.monticore.lang.sysml.sysml._symboltable.SysMLGlobalScope;
 import de.monticore.lang.sysml.sysml._symboltable.SysMLLanguage;
+import de.monticore.lang.sysml.sysml._symboltable.serialization.SysMLScopeDeSer;
 import de.se_rwth.commons.logging.Log;
 
 import java.io.File;
@@ -18,6 +20,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,6 +29,8 @@ import java.util.stream.Stream;
  * @version 1.0
  */
 public class SysMLTool {
+  public static final String DEFAULT_SYMBOL_LOCATION = "target";
+
   public static void main(String[] args) {
     if (args.length != 1) {
       Log.error("Please specify only one single path to the input directory containing the input models.");
@@ -34,16 +39,26 @@ public class SysMLTool {
     String dir = args[0];
     SysMLParserMultipleFiles sysMLParserMultipleFiles = new SysMLParserMultipleFiles();
     File checkingIfDirOrFileExists = new File(dir);
-    if(!checkingIfDirOrFileExists.exists()){
+    if (!checkingIfDirOrFileExists.exists()) {
       Log.error("The provided input path " + dir + " does not exists. Exiting.");
       return;
     }
 
     Log.info("Parsing models in directory or file " + dir, SysMLTool.class.getName());
+    // Gathering all models in given directory "dir".
     List<ASTUnit> models = new ArrayList<>();
     List<String> filePaths = getSysMLFilePathsInDirectory(dir);
+    Set<Path> p = Sets.newHashSet();
+    for (String mP : filePaths) {
+      File f = new File(mP);
+      p.add(Paths.get(f.getAbsolutePath()));
+    }
+    final ModelPath mp = new ModelPath(p);
+
+    // Parsing
+
     try {
-      for(String path: filePaths){
+      for (String path : filePaths) {
         models.add(sysMLParserMultipleFiles.parseSingleFile(path));
       }
     }
@@ -53,36 +68,23 @@ public class SysMLTool {
       return;
     }
 
+    // Symboltable
     Log.info("Creating Symbol Table.", SysMLTool.class.getName());
-    //TODO Finding: 0xA6100x1387190359 GlobalScope
-    // SysMLGlobalScope has no EnclosingScope, so you cannot call methodgetEnclosingScope
-    /*HelperSysMLSymbolTableCreator symbolTableHelper = new HelperSysMLSymbolTableCreator();
-    SysMLLanguage sysMLLanguage = new SysMLLanguageSub("SysML", ".sysml");
-    for (ASTUnit astUnit : models) {
-      symbolTableHelper.buildSymbolTableWithGlobalScope(astUnit, new ModelPath(), sysMLLanguage);
-    }*/
 
-    //Setup Symboltable TODO
-    //SysMLLanguage language = new SysMLLanguage();
-    HelperSysMLSymbolTableCreator symbolTableHelper = new HelperSysMLSymbolTableCreator();
-    SysMLLanguage sysMLLanguage = new SysMLLanguageSub("SysML", ".sysml");
-    for (ASTUnit astUnit : models) {
-      SysMLArtifactScope scope = new SysMLArtifactScope("", new ArrayList<>());
-      SysMLArtifactScope extendedScope = symbolTableHelper.buildSymbolTable(astUnit, scope);
-    }
+    SysMLLanguageSub sysMLLanguage = new SysMLLanguageSub("SysML", ".sysml");
+    SysMLGlobalScope montiArcGlobalScope = new SysMLGlobalScope(mp, sysMLLanguage);
 
+    // Context Conditions
     Log.info("Checking Context Conditions.", SysMLTool.class.getName());
     for (ASTUnit astUnit : models) {
       runDefaultCocos(astUnit);
     }
 
-
-
     Log.info("Parsed and checked all models successfully.", SysMLTool.class.getName());
 
   }
 
-  public static List<String> getSysMLFilePathsInDirectory (String dir) {
+  public static List<String> getSysMLFilePathsInDirectory(String dir) {
     try (Stream<Path> walk = Files.walk(Paths.get(dir))) {
 
       List<String> result = walk.filter(Files::isRegularFile).map(x -> x.toString()).collect(Collectors.toList());
@@ -103,7 +105,7 @@ public class SysMLTool {
     }
   }
 
-    public static void runDefaultCocos(ASTUnit unit){
+  public static void runDefaultCocos(ASTUnit unit) {
     SysMLCoCos cocos = new SysMLCoCos();
     cocos.getCheckerForAllCoCos().checkAll(unit);
   }
