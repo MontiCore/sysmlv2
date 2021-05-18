79a2c85c84e75164bedc36776844c5c46fd2dcea
Adrian- Costin Marin <adrian.costin.marin@rwth-aachen.de>
2021-05-18 11:42:50 +0000

    Allow Multiple Top-level Packages

--- src/main/java/de/monticore/lang/sysml/cocos/naming/ArtifactStartsWithPackage.java
+++ src/main/java/de/monticore/lang/sysml/cocos/naming/ArtifactStartsWithPackage.java
@@ -2,7 +2,7 @@ package de.monticore.lang.sysml.cocos.naming;
 
 import de.monticore.lang.sysml.basics.interfaces.sysmlshared._ast.ASTUnit;
 import de.monticore.lang.sysml.basics.interfaces.sysmlshared._cocos.SysMLSharedASTUnitCoCo;
-import de.monticore.lang.sysml.basics.sysmldefault.sysmlimportsandpackages._ast.ASTPackageUnit;
+import de.monticore.lang.sysml.basics.sysmldefault.sysmlimportsandpackages._ast.ASTRootNamespace;
 import de.monticore.lang.sysml.cocos.SysMLCoCoName;
 import de.monticore.lang.sysml.cocos.SysMLCoCos;
 import de.se_rwth.commons.logging.Log;
@@ -14,7 +14,7 @@ import de.se_rwth.commons.logging.Log;
 public class ArtifactStartsWithPackage implements SysMLSharedASTUnitCoCo {
   @Override
   public void check(ASTUnit node) {
-    if(!(node instanceof ASTPackageUnit)){
+    if(!(node instanceof ASTRootNamespace)){
       Log.warn(SysMLCoCos.getErrorCode(SysMLCoCoName.ArtifactStartsWithPackage) +
               " Each artifact should start with its a package to organize the model elements." ,
           node.get_SourcePositionStart());
