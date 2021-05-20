72d703e828cb10513d6a8124ed43c2b9b795881e
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-05-20 08:30:07 +0200

    Complete SysML v2 grammar rework.

---
+++ src/main/java/de/monticore/lang/sysml/sysmlblockdiagrams/_symboltable/AttributeDefSymbolDeSer.java
@@ -0,0 +1,18 @@
+package de.monticore.lang.sysml.sysmlblockdiagrams._symboltable;
+
+import de.monticore.symboltable.serialization.json.JsonObject;
+import de.monticore.types.check.SymTypeExpression;
+
+import java.util.List;
+
+public class AttributeDefSymbolDeSer extends AttributeDefSymbolDeSerTOP {
+  @Override
+  protected void serializeSuperTypes(List<SymTypeExpression> superTypes, SysMLBlockDiagramsSymbols2Json s2j) {
+
+  }
+
+  @Override
+  protected List<SymTypeExpression> deserializeSuperTypes(JsonObject symbolJson) {
+    return null;
+  }
+}
---
+++ src/main/java/de/monticore/lang/sysml/sysmlblockdiagrams/_symboltable/SysMLPortDefSymbolDeSer.java
@@ -0,0 +1,18 @@
+package de.monticore.lang.sysml.sysmlblockdiagrams._symboltable;
+
+import de.monticore.symboltable.serialization.json.JsonObject;
+import de.monticore.types.check.SymTypeExpression;
+
+import java.util.List;
+
+public class SysMLPortDefSymbolDeSer extends SysMLPortDefSymbolDeSerTOP {
+  @Override
+  protected void serializeSuperTypes(List<SymTypeExpression> superTypes, SysMLBlockDiagramsSymbols2Json s2j) {
+
+  }
+
+  @Override
+  protected List<SymTypeExpression> deserializeSuperTypes(JsonObject symbolJson) {
+    return null;
+  }
+}
