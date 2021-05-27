a0d75185a63a993e012587cfe760f83e743e261f
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-05-27 17:42:49 +0200

    Improve instantiations wrt. qualified names and add corresponding test.

--- src/test/java/de/monticore/lang/sysmlv2/_parser/SysMLv2CustomParserTest.java
+++ src/test/java/de/monticore/lang/sysmlv2/_parser/SysMLv2CustomParserTest.java
@@ -16,11 +16,11 @@ import static org.junit.Assert.assertTrue;
 
 public class SysMLv2CustomParserTest {
 
-  private static final String INTRO_PATH = "src/test/resources/parser/custom/";
+  private static final String MODEL_PATH = "src/test/resources/parser/custom/";
 
   @Test
   public void testACDefInACDef() throws IOException {
-    Path model = Paths.get(INTRO_PATH + "acdef_in_def.sysml");
+    Path model = Paths.get(MODEL_PATH + "acdef_in_def.sysml");
 
     SysMLv2Mill.init();
     SysMLv2Parser parser = SysMLv2Mill.parser();
@@ -32,7 +32,7 @@ public class SysMLv2CustomParserTest {
 
   @Test
   public void testOpaqueACDefInACDef() throws IOException {
-    Path model = Paths.get(INTRO_PATH + "opaque_acdef_in_def.sysml");
+    Path model = Paths.get(MODEL_PATH + "opaque_acdef_in_def.sysml");
 
     SysMLv2Mill.init();
     SysMLv2Parser parser = SysMLv2Mill.parser();
@@ -44,7 +44,7 @@ public class SysMLv2CustomParserTest {
 
   @Test
   public void testACDefInACElement() throws IOException {
-    Path model = Paths.get(INTRO_PATH + "acdef_in_element.sysml");
+    Path model = Paths.get(MODEL_PATH + "acdef_in_element.sysml");
 
     SysMLv2Mill.init();
     SysMLv2Parser parser = SysMLv2Mill.parser();
@@ -56,7 +56,19 @@ public class SysMLv2CustomParserTest {
 
   @Test
   public void testOpaqueACDefInACElement() throws IOException {
-    Path model = Paths.get(INTRO_PATH + "opaque_acdef_in_element.sysml");
+    Path model = Paths.get(MODEL_PATH + "opaque_acdef_in_element.sysml");
+
+    SysMLv2Mill.init();
+    SysMLv2Parser parser = SysMLv2Mill.parser();
+    Optional<ASTSysMLModel> ast = parser.parse(model.toString());
+
+    assertFalse(parser.hasErrors());
+    assertTrue(ast.isPresent());
+  }
+
+  @Test
+  public void testQualifiedSend() throws IOException {
+    Path model = Paths.get(MODEL_PATH + "qualified_send.sysml");
 
     SysMLv2Mill.init();
     SysMLv2Parser parser = SysMLv2Mill.parser();
