d705d24fbb5ca1dae9487cb83980559dd94d3fd0
Mathias Pfeiffer <mpfeiffer@se-rwth.de>
2021-07-31 15:26:53 +0200

    Merge branch 'grammars' into master

---
+++ src/main/java/de/monticore/lang/sysmlblockdiagrams/_symboltable/EnumDefSymbolDeSer.java
@@ -0,0 +1,19 @@
+package de.monticore.lang.sysmlblockdiagrams._symboltable;
+
+import de.monticore.symboltable.serialization.json.JsonObject;
+import de.monticore.types.check.SymTypeExpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnumDefSymbolDeSer extends EnumDefSymbolDeSerTOP {
+  @Override
+  protected void serializeSuperTypes(List<SymTypeExpression> superTypes, SysMLBlockDiagramsSymbols2Json s2j) {
+
+  }
+
+  @Override
+  protected List<SymTypeExpression> deserializeSuperTypes(JsonObject symbolJson) {
+    return new ArrayList<>();
+  }
+}
