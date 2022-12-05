506166fa9b5e5d45f8e6e1ed7c989143cc78d51d
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2022-12-05 16:34:06 +0100

    added actionGenerator CoCos

--- language/src/test/java/cocos/ActionCoCosTest.java
+++ language/src/test/java/cocos/ActionCoCosTest.java
@@ -1,16 +1,22 @@
 package cocos;
 
 import de.monticore.lang.sysmlactions._cocos.SysMLActionsASTActionDefCoCo;
+import de.monticore.lang.sysmlactions._cocos.SysMLActionsASTActionUsageCoCo;
 import de.monticore.lang.sysmlv2.SysMLv2Mill;
 import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
 import de.monticore.lang.sysmlv2._cocos.SysMLv2CoCoChecker;
 import de.monticore.lang.sysmlv2._parser.SysMLv2Parser;
+import de.monticore.lang.sysmlv2.cocos.ActionGeneratorCoCos;
+import de.monticore.lang.sysmlv2.cocos.ActionNameCoCos;
 import de.monticore.lang.sysmlv2.cocos.ActionSupertypes;
 import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 
@@ -19,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ActionCoCosTest {
 
-  private static final String MODEL_PATH = "src/test/resources/parser";
+  private static final String MODEL_PATH = "src/test/resources/cocos/actions";
 
   private SysMLv2Parser parser = SysMLv2Mill.parser();
 
@@ -39,14 +45,31 @@ public class ActionCoCosTest {
   @Nested
   public class ActionDef {
 
-    @Test
-    public void testValid() throws IOException {
-      ASTSysMLModel ast = SysMLv2Mill.parser().parse_String("action def A; action def B: A;").get();
-      SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
-      var checker = new SysMLv2CoCoChecker();
-      checker.addCoCo((SysMLActionsASTActionDefCoCo) new ActionSupertypes());
-      checker.checkAll(ast);
-      assertTrue(Log.getFindings().isEmpty());
+    @ParameterizedTest(name = "{index} - {0} does pass all checks w/o errors")
+    @ValueSource(strings = {
+        "0_valid.sysml", // example with action usage
+        "1_valid.sysml", // example with control action usages
+    })
+    public void testValid(String modelName) throws IOException {
+      var optAst = SysMLv2Mill.parser().parse(MODEL_PATH + "/" + modelName);
+      if(optAst.isPresent()) {
+        ASTSysMLModel ast = optAst.get();
+
+        SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+        var checker = new SysMLv2CoCoChecker();
+        checker.addCoCo((SysMLActionsASTActionDefCoCo) new ActionSupertypes());
+        checker.addCoCo((SysMLActionsASTActionDefCoCo) new ActionNameCoCos());
+
+        checker.addCoCo((SysMLActionsASTActionUsageCoCo) new ActionGeneratorCoCos());
+
+        checker.addCoCo((SysMLActionsASTActionUsageCoCo) new ActionSupertypes());
+        checker.addCoCo((SysMLActionsASTActionUsageCoCo) new ActionNameCoCos());
+        checker.checkAll(ast);
+        assertTrue(Log.getFindings().isEmpty());
+      }
+      else {
+        Assertions.fail("not parsable");
+      }
     }
 
     @Test
