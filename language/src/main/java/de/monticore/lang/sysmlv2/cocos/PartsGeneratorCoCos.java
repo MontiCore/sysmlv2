4bd40eff5be1a72256e8176c6005804a60627517
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2022-12-19 14:38:06 +0100

    changed parts generator coco

--- language/src/main/java/de/monticore/lang/sysmlv2/cocos/PartsGeneratorCoCos.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/PartsGeneratorCoCos.java
@@ -1,5 +1,9 @@
 package de.monticore.lang.sysmlv2.cocos;
 
+import de.monticore.lang.sysmlactions._ast.ASTActionDef;
+import de.monticore.lang.sysmlactions._ast.ASTActionUsage;
+import de.monticore.lang.sysmlbasis._ast.ASTSysMLSpecialization;
+import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
 import de.monticore.lang.sysmlparts._ast.ASTPartUsage;
 import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTPartUsageCoCo;
 import de.se_rwth.commons.logging.Log;
@@ -9,12 +13,15 @@ import java.util.stream.Collectors;
 public class PartsGeneratorCoCos implements SysMLPartsASTPartUsageCoCo {
 
   /**
-   * Check that at least one part def is extende.
+   * Check that at least one part def is extended.
    */
   @Override public void check(ASTPartUsage node) {
-    var specialications = node.streamSpecializations().flatMap(s -> s.streamSuperTypes()).collect(Collectors.toList());
-
-    if(specialications.isEmpty())
+    var specialications = node.streamSpecializations().filter(t -> t instanceof ASTSysMLSpecialization).collect(
+        Collectors.toList());
+    var relevantElements = node.getSysMLElementList().stream().filter(
+        t -> t instanceof ASTActionDef | t instanceof ASTActionUsage | t instanceof ASTAttributeUsage
+            | t instanceof ASTPartUsage).collect(Collectors.toList()).size(); //partUsage with at least one of the types is seen as a adhoc class definition
+    if(specialications.isEmpty() && relevantElements == 0)
       Log.error("A Part Usage has to extend at least one part def");
   }
 }
