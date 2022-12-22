a6cf53502cb74ff3855524cbe3460257eef20598
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2022-12-22 17:06:26 +0100

    added AttributeGeneratorCocos and test

--- language/src/main/java/de/monticore/lang/sysmlv2/cocos/AttributeGeneratorCoCos.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/AttributeGeneratorCoCos.java
@@ -2,8 +2,10 @@ package de.monticore.lang.sysmlv2.cocos;
 
 import de.monticore.lang.sysmlbasis._ast.ASTSpecialization;
 import de.monticore.lang.sysmlbasis._ast.ASTSysMLRedefinition;
+import de.monticore.lang.sysmlbasis._ast.ASTSysMLSpecialization;
 import de.monticore.lang.sysmlbasis._ast.ASTSysMLTyping;
 import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
+import de.monticore.lang.sysmlparts._ast.ASTPartDef;
 import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTAttributeUsageCoCo;
 import de.monticore.lang.sysmlv2.types.SysMLBasisTypesFullPrettyPrinter;
 import de.monticore.prettyprint.IndentPrinter;
@@ -23,45 +25,94 @@ public class AttributeGeneratorCoCos implements SysMLPartsASTAttributeUsageCoCo 
    */
   @Override
   public void check(ASTAttributeUsage node) {
-    checkTyping(node);
+    checkTypingExists(node);
     var redefinitionList = node.getSpecializationList().stream().filter(t -> t instanceof ASTSysMLRedefinition).collect(
         Collectors.toList());
     for (ASTSpecialization redefinition : redefinitionList) {
-      if(((ASTSysMLRedefinition) redefinition).getSuperTypesList().size() != 1) {
+      if(redefinition.getSuperTypesList().size() != 1) {
         Log.error("Attribute Usage " + node.getName() + " has "
-            + ((ASTSysMLRedefinition) redefinition).getSuperTypesList().size()
+            + redefinition.getSuperTypesList().size()
             + " redefinitions, but may only redefine 1.");
       }
-      var redefinitionType = ((ASTSysMLRedefinition) redefinition).getSuperTypes(0); //only one redefinition is allowed
+      String parentAttributeName = printName(redefinition.getSuperTypes(0));
 
-      var definition = node.getEnclosingScope().resolveAttributeDef(printName(redefinitionType));
-      if(definition.isEmpty()) {
-
-        var usage = node.getEnclosingScope().resolveAttributeUsage(printName(redefinitionType));
-
-        if(usage.isPresent()) {
-          var usageType = usage.getClass(); //attributes may only have 1 type
-          var nodeType = ((ASTSysMLTyping) node.getSpecializationList().stream().filter(
-              t -> t instanceof ASTSysMLTyping).collect(
-              Collectors.toList()).get(0)); //attributes may only have 1 type
-
-          //TODO compare nodeType and usageType for compatibility
-        }
-        else {
-          Log.error("Attribute Usage redefinition" + node.getName() + " could not be found");
-        }
+      if(!node.getName().equals(parentAttributeName)) {
+        Log.error("Attribute Usage " + node.getName()
+            + " has to redefine an Attribute Usage with the same name, but redefines "
+            + parentAttributeName + ".");
       }
-
+      checkAttributeDefined(node, parentAttributeName);
     }
+
   }
 
-  void checkTyping(ASTAttributeUsage node) {
-    int typeCount = node.getSpecializationList().stream().filter(t -> t instanceof ASTSysMLTyping).collect(
-        Collectors.toList()).size();
+  void checkTypingExists(ASTAttributeUsage node) {
+    long typeCount = node.getSpecializationList().stream().filter(t -> t instanceof ASTSysMLTyping).count();
     if(typeCount != 1) {
       Log.error("Attribute usage " + node.getName() + " has " + typeCount + " typings, but needs exactly 1.");
+    }
+  }
+
+  void checkAttributeDefined(ASTAttributeUsage attributeUsage, String redefinedAttributeName) {
+    int attributeFoundInParents = 0; // we count how often the redefinedAttribute is defined within the transitiveSupertypes of the parent node
+    var parentNode = attributeUsage.getEnclosingScope().getAstNode();
+
+    if(parentNode instanceof ASTPartDef) {
+      for (ASTPartDef transSuperType : ((ASTPartDef) parentNode).getTransitiveSupertypes()) {
+        var attribute = transSuperType.getSpannedScope().resolveAttributeUsage(redefinedAttributeName);
+        if(attribute.isPresent()) {
+          ASTAttributeUsage refinedAttr = attribute.get().getAstNode();
+          if(refinedAttr.getSpecializationList().stream().filter(
+              t -> t instanceof ASTSysMLRedefinition).count() == 0) //we only count attributeUsages where no redefinition is used
+          {
+            attributeFoundInParents++;
+          }
+
+          checkTypeCompatibility(attributeUsage, refinedAttr);
+
+        }
+      }
+      if(attributeFoundInParents != 1)
+        Log.error("Attribute usage " + attributeUsage.getName() + " was found " + attributeFoundInParents
+            + " in the transitive super types of its parent, but only 1 is allowed.");
 
     }
-
   }
+
+  private void checkTypeCompatibility(ASTAttributeUsage attributeUsage, ASTAttributeUsage redefinedAttribute) {
+    var attributeUsageType = attributeUsage.getSpecializationList().stream().filter(
+        t -> t instanceof ASTSysMLTyping).collect(
+        Collectors.toList()).get(0).getSuperTypes(0);
+    var refinedUsageType = redefinedAttribute.getSpecializationList().stream().filter(
+        t -> t instanceof ASTSysMLTyping).collect(
+        Collectors.toList()).get(0).getSuperTypes(0);
+    var attributeType = attributeUsage.getEnclosingScope().resolveAttributeDef(
+        printName(attributeUsageType));
+
+    var refinedType = redefinedAttribute.getEnclosingScope().resolveAttributeDef(
+        printName(refinedUsageType));
+
+    if(attributeType.isEmpty()) {
+      Log.error(
+          "The type " + printName(attributeUsageType) + " of the Attribute usage " + attributeUsage.getName()
+              + " was not resolvable.");
+    }
+    if(refinedType.isEmpty()) {
+      Log.error(
+          "The type " + printName(refinedUsageType) + " of the Attribute usage " + redefinedAttribute.getName()
+              + " was not resolvable.");
+    }
+    var specializationStringList = attributeType.get().getAstNode()
+        .streamSpecializations()
+        .filter(t -> t instanceof ASTSysMLSpecialization)
+        .flatMap(s -> s.streamSuperTypes())
+        .map(s -> printName(s))
+        .collect(Collectors.toList());
+    if(!specializationStringList.contains(printName(refinedUsageType))) {
+      Log.error(
+          "The type " + printName(attributeUsageType) + " is not a a subtype of " + printName(refinedUsageType) + " .");
+
+    }
+  }
+
 }
---
+++ language/src/test/java/cocos/AttributeGeneratorCoCosTest.java
@@ -0,0 +1,61 @@
+package cocos;
+
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTAttributeUsageCoCo;
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.lang.sysmlv2._cocos.SysMLv2CoCoChecker;
+import de.monticore.lang.sysmlv2._parser.SysMLv2Parser;
+import de.monticore.lang.sysmlv2._visitor.SysMLv2Traverser;
+import de.monticore.lang.sysmlv2.cocos.AttributeGeneratorCoCos;
+import de.monticore.lang.sysmlv2.visitor.PartsTransitiveVisitor;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AttributeGeneratorCoCosTest {
+
+  private static final String MODEL_PATH = "src/test/resources/cocos/attribute";
+
+  @BeforeAll
+  public static void init() {
+    Log.init();
+    SysMLv2Mill.init();
+  }
+
+  @BeforeEach
+  public void reset() {
+    SysMLv2Mill.globalScope().clear();
+    Log.getFindings().clear();
+    Log.enableFailQuick(true);
+  }
+
+    @ParameterizedTest(name = "{index} - {0} does pass all checks w/o errors")
+    @ValueSource(strings = {
+        "0_valid.sysml" // example for redefinition
+    })
+    public void testValid(String modelName) throws IOException {
+      var optAst = SysMLv2Mill.parser().parse(MODEL_PATH + "/" + modelName);
+      if(optAst.isPresent()) {
+        ASTSysMLModel ast = optAst.get();
+
+        SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+        SysMLv2Traverser sysMLv2Traverser = SysMLv2Mill.traverser();
+        sysMLv2Traverser.add4SysMLParts(new PartsTransitiveVisitor());
+        sysMLv2Traverser.handle(ast);
+        var checker = new SysMLv2CoCoChecker();
+        checker.addCoCo( new AttributeGeneratorCoCos());
+        checker.checkAll(ast);
+        assertTrue(Log.getFindings().isEmpty());
+      }
+      else {
+        Assertions.fail("not parsable");
+      }
+    }
+
+
+}
