694deb664a9026e219c4115a66ac3c5a94e0cc3a
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-01-10 10:52:02 +0100

    changed order of actions in state def and state usage

--- language/src/main/java/de/monticore/lang/sysmlstates/cocos/NoDoActions.java
+++ language/src/main/java/de/monticore/lang/sysmlstates/cocos/NoDoActions.java
@@ -1,10 +1,8 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.lang.sysmlstates.cocos;
 
-import de.monticore.lang.sysmlstates._ast.ASTDoAction;
 import de.monticore.lang.sysmlstates._ast.ASTStateDef;
 import de.monticore.lang.sysmlstates._ast.ASTStateUsage;
-import de.monticore.lang.sysmlstates._cocos.SysMLStatesASTDoActionCoCo;
 import de.monticore.lang.sysmlstates._cocos.SysMLStatesASTStateDefCoCo;
 import de.monticore.lang.sysmlstates._cocos.SysMLStatesASTStateUsageCoCo;
 import de.se_rwth.commons.logging.Log;
@@ -16,14 +14,14 @@ public class NoDoActions implements SysMLStatesASTStateUsageCoCo, SysMLStatesAST
 
   @Override
   public void check(ASTStateUsage node) {
-    if(node.isPresentDoAction()) {
+    if(!node.getDoActionList().isEmpty()) {
       Log.error("DoActions are not supported.", node.get_SourcePositionStart(), node.get_SourcePositionEnd());
     }
   }
 
   @Override
   public void check(ASTStateDef node) {
-    if(node.isPresentDoAction()) {
+    if(!node.getDoActionList().isEmpty()) {
       Log.error("DoActions are not supported.", node.get_SourcePositionStart(), node.get_SourcePositionEnd());
     }
   }
