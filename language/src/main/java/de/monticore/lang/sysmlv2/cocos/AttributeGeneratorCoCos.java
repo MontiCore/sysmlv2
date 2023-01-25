a3c8824f3718361202ea6b9116d7e97139255da2
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-01-25 19:40:28 +0100

    reworked AttributeGeneratorCoCos to correctly check if the transitive supertypes of the parent implement the attribute usage

--- language/src/main/java/de/monticore/lang/sysmlv2/cocos/AttributeGeneratorCoCos.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/AttributeGeneratorCoCos.java
@@ -6,12 +6,17 @@ import de.monticore.lang.sysmlbasis._ast.ASTSysMLSpecialization;
 import de.monticore.lang.sysmlbasis._ast.ASTSysMLTyping;
 import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
 import de.monticore.lang.sysmlparts._ast.ASTPartDef;
+import de.monticore.lang.sysmlparts._ast.ASTPartUsage;
 import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTAttributeUsageCoCo;
+import de.monticore.lang.sysmlparts._symboltable.AttributeUsageSymbol;
 import de.monticore.lang.sysmlv2.types.SysMLBasisTypesFullPrettyPrinter;
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.types.mcbasictypes._ast.ASTMCType;
 import de.se_rwth.commons.logging.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class AttributeGeneratorCoCos implements SysMLPartsASTAttributeUsageCoCo {
@@ -28,20 +33,37 @@ public class AttributeGeneratorCoCos implements SysMLPartsASTAttributeUsageCoCo 
     checkTypingExists(node);
     var redefinitionList = node.getSpecializationList().stream().filter(t -> t instanceof ASTSysMLRedefinition).collect(
         Collectors.toList());
-    for (ASTSpecialization redefinition : redefinitionList) {
-      if(redefinition.getSuperTypesList().size() != 1) {
-        Log.error("Attribute Usage " + node.getName() + " has "
-            + redefinition.getSuperTypesList().size()
-            + " redefinitions, but may only redefine 1.");
-      }
-      String parentAttributeName = printName(redefinition.getSuperTypes(0));
-
-      if(!node.getName().equals(parentAttributeName)) {
+    List<ASTAttributeUsage> attributeUsageList = checkAttributeDefinedInParents(node);
+    long redefinitionCount = attributeUsageList.stream().flatMap(t -> t.getSpecializationList().stream()).filter(
+        f -> f instanceof ASTSysMLRedefinition).count(); //counts the number of redefinitions
+    //TODO CHECKPARENTS Optional<AttributeUsage raus> und dann check ich ob das mit redefinitions und type passt
+    if(redefinitionList.isEmpty()) {
+      if(attributeUsageList.size() != 0)
         Log.error("Attribute Usage " + node.getName()
-            + " has to redefine an Attribute Usage with the same name, but redefines "
-            + parentAttributeName + ".");
+            + " is already defined in the transitive supertypes of the parent element. Attribute Usages with the same name where found "
+            + attributeUsageList.size() + " times.");
+    }
+    else {
+      for (ASTSpecialization redefinition : redefinitionList) {
+        if(redefinition.getSuperTypesList().size() != 1) {
+          Log.error("Attribute Usage " + node.getName() + " has "
+              + redefinition.getSuperTypesList().size()
+              + " redefinitions, but may only redefine 1.");
+        }
+        if(attributeUsageList.size() != (1 + redefinitionCount)) {
+          Log.error("Attribute Usage " + node.getName()
+              + " has to be defined exactly once in the transitive supertypes of the parent element. Attribute Usages with the same name where found "
+              + attributeUsageList.size() + " times.");
+        }
+        String parentAttributeName = printName(redefinition.getSuperTypes(0));
+
+        if(!node.getName().equals(parentAttributeName)) {
+          Log.error("Attribute Usage " + node.getName()
+              + " has to redefine an Attribute Usage with the same name, but redefines "
+              + parentAttributeName + ".");
+        }
+        checkTypeCompatibility(node, attributeUsageList.get(0));
       }
-      checkAttributeDefined(node, parentAttributeName);
     }
 
   }
@@ -53,30 +75,28 @@ public class AttributeGeneratorCoCos implements SysMLPartsASTAttributeUsageCoCo 
     }
   }
 
-  void checkAttributeDefined(ASTAttributeUsage attributeUsage, String redefinedAttributeName) {
-    int attributeFoundInParents = 0; // we count how often the redefinedAttribute is defined within the transitiveSupertypes of the parent node
+  List<ASTAttributeUsage> checkAttributeDefinedInParents(ASTAttributeUsage attributeUsage) {
+    List<ASTAttributeUsage> attributeUsageList = new ArrayList<>();
     var parentNode = attributeUsage.getEnclosingScope().getAstNode();
-
+    String usageName = attributeUsage.getName();
+    Optional<AttributeUsageSymbol> astAttributeUsage;
     if(parentNode instanceof ASTPartDef) {
       for (ASTPartDef transSuperType : ((ASTPartDef) parentNode).getTransitiveDefSupertypes()) {
-        var attribute = transSuperType.getSpannedScope().resolveAttributeUsage(redefinedAttributeName);
-        if(attribute.isPresent()) {
-          ASTAttributeUsage refinedAttr = attribute.get().getAstNode();
-          if(refinedAttr.getSpecializationList().stream().filter(
-              t -> t instanceof ASTSysMLRedefinition).count() == 0) //we only count attributeUsages where no redefinition is used
-          {
-            attributeFoundInParents++;
-          }
-
-          checkTypeCompatibility(attributeUsage, refinedAttr);
-
-        }
+        astAttributeUsage = transSuperType.getSpannedScope().resolveAttributeUsage(usageName);
+        astAttributeUsage.ifPresent(attributeUsageSymbol -> attributeUsageList.add(attributeUsageSymbol.getAstNode()));
       }
-      if(attributeFoundInParents != 1)
-        Log.error("Attribute usage " + attributeUsage.getName() + " was found " + attributeFoundInParents
-            + " in the transitive super types of its parent, but only 1 is allowed.");
-
     }
+    if(parentNode instanceof ASTPartUsage) {
+      for (ASTPartDef transSuperType : ((ASTPartUsage) parentNode).getTransitiveDefSupertypes()) {
+        astAttributeUsage = transSuperType.getSpannedScope().resolveAttributeUsage(usageName);
+        astAttributeUsage.ifPresent(attributeUsageSymbol -> attributeUsageList.add(attributeUsageSymbol.getAstNode()));
+      }
+      for (ASTPartUsage transSuperType : ((ASTPartUsage) parentNode).getTransitiveUsageSupertypes()) {
+        astAttributeUsage = transSuperType.getSpannedScope().resolveAttributeUsage(usageName);
+        astAttributeUsage.ifPresent(attributeUsageSymbol -> attributeUsageList.add(attributeUsageSymbol.getAstNode()));
+      }
+    }
+    return attributeUsageList;
   }
 
   private void checkTypeCompatibility(ASTAttributeUsage attributeUsage, ASTAttributeUsage redefinedAttribute) {
