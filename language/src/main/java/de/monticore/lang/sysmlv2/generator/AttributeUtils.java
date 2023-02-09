71b7e35507018bb33a159a277a5d255ecdbcfe73
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-02-09 13:45:49 +0100

    AttributeUtils refactored to include methods from AttributeResolveUtils

--- language/src/main/java/de/monticore/lang/sysmlv2/generator/AttributeUtils.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/generator/AttributeUtils.java
@@ -3,81 +3,39 @@ package de.monticore.lang.sysmlv2.generator;
 import de.monticore.cd4code.CD4CodeMill;
 import de.monticore.cdbasis._ast.ASTCDAttribute;
 import de.monticore.lang.sysmlbasis._ast.ASTSysMLElement;
-import de.monticore.lang.sysmlparts._ast.ASTAttributeDef;
 import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
-import de.monticore.lang.sysmlparts._ast.ASTPartDef;
-import de.monticore.lang.sysmlparts._ast.ASTPartUsage;
 import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedType;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class AttributeUtils {
 
   GeneratorUtils generatorUtils;
 
-  public AttributeUtils(){
+  AttributeResolveUtils attributeResolveUtils;
+
+  public AttributeUtils() {
     this.generatorUtils = new GeneratorUtils();
+    this.attributeResolveUtils = new AttributeResolveUtils();
   }
 
   List<ASTCDAttribute> createAttributes(ASTSysMLElement astSysMLElement) {
-    List<ASTAttributeUsage> attributeUsageList = createAttributeUsageList(astSysMLElement);
-    List<String> generatedAttributeList = new ArrayList<>();
+    List<ASTAttributeUsage> attributeUsageList = attributeResolveUtils.getAttributesOfElement(astSysMLElement);
     //create astcdattributes for the current element
-    List<ASTCDAttribute> attributeList = attributeUsageList.stream().map(
-        t -> createAttribute(t, generatedAttributeList)).collect(
+    return attributeUsageList.stream().map(
+        t -> createAttribute(t)).collect(
         Collectors.toList());
-    attributeList.removeAll(Collections.singleton(null));
-    Stream<ASTAttributeUsage> attributeUsageStream = null;
-    //create astcdattributes for transitive attributes
-    if(astSysMLElement instanceof ASTPartDef) {
-      attributeUsageStream = ((ASTPartDef) astSysMLElement).streamTransitiveDefSupertypes().flatMap(
-          t -> createAttributeUsageList(t).stream());
-    }
-    if(astSysMLElement instanceof ASTAttributeDef) {
-      attributeUsageStream = ((ASTAttributeDef) astSysMLElement).streamTransitiveDefSupertypes().flatMap(
-          t -> createAttributeUsageList(t).stream());
-    }
-    if(astSysMLElement instanceof ASTPartUsage) {
-      attributeUsageStream = ((ASTPartUsage) astSysMLElement).streamTransitiveDefSupertypes().flatMap(
-          t -> createAttributeUsageList(t).stream());
-    }
-    List<ASTCDAttribute> supertypeAttributeList = attributeUsageStream.map(
-        f -> createAttribute(f, generatedAttributeList)).collect(
-        Collectors.toList());
-
-    supertypeAttributeList.removeAll(Collections.singleton(null));
-    attributeList.addAll(supertypeAttributeList);
-    return attributeList;
   }
 
-
-  private List<ASTAttributeUsage> createAttributeUsageList(ASTSysMLElement element) {
-    List<ASTSysMLElement> elementList = new ArrayList<>();
-    if(element instanceof ASTPartDef)
-      elementList = ((ASTPartDef) element).getSysMLElementList();
-    if(element instanceof ASTPartUsage)
-      elementList = ((ASTPartUsage) element).getSysMLElementList();
-    if(element instanceof ASTAttributeDef)
-      elementList = ((ASTAttributeDef) element).getSysMLElementList();
-    List<ASTAttributeUsage> attributeUsageList;
-    attributeUsageList = elementList.stream().filter(
-        t -> t instanceof ASTAttributeUsage).map(t -> (ASTAttributeUsage) t).collect(Collectors.toList());
-    return attributeUsageList;
-  }
-
-  ASTCDAttribute createAttribute(ASTSysMLElement element, List<String> stringList) {
+  ASTCDAttribute createAttribute(ASTSysMLElement element) {
     if(element instanceof ASTAttributeUsage) {
       String attributeName = ((ASTAttributeUsage) element).getName();
-      if(!stringList.contains(attributeName)) {
-        stringList.add(attributeName);
-        ASTMCQualifiedType qualifiedType = generatorUtils.attributeType((ASTAttributeUsage) element);
-        return CD4CodeMill.cDAttributeBuilder().setName(attributeName).setModifier(
-            CD4CodeMill.modifierBuilder().PUBLIC().build()).setMCType(qualifiedType).build();
-      }
+
+      ASTMCQualifiedType qualifiedType = generatorUtils.attributeType((ASTAttributeUsage) element);
+      return CD4CodeMill.cDAttributeBuilder().setName(attributeName).setModifier(
+          CD4CodeMill.modifierBuilder().PUBLIC().build()).setMCType(qualifiedType).build();
+
     }
     return null;
   }
