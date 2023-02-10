94b41c4ddbe27fcd06b10e1bf20af3f280f99e13
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-02-10 20:12:57 +0100

    remove necessity for flow type

--- language/src/main/java/de/monticore/lang/sysmlv2/cocos/ConnectionGeneratorCoCos.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/ConnectionGeneratorCoCos.java
@@ -1,11 +1,8 @@
 package de.monticore.lang.sysmlv2.cocos;
 
-
 import de.monticore.lang.sysmlconnections._ast.ASTFlow;
 import de.monticore.lang.sysmlconnections._cocos.SysMLConnectionsASTFlowCoCo;
 import de.monticore.lang.sysmlparts._ast.ASTPartDef;
-import de.se_rwth.commons.logging.Log;
-
 
 public class ConnectionGeneratorCoCos implements SysMLConnectionsASTFlowCoCo {
   /**
@@ -13,13 +10,9 @@ public class ConnectionGeneratorCoCos implements SysMLConnectionsASTFlowCoCo {
    */
   @Override public void check(ASTFlow node) {
 
-    if(!node.isPresentFlowtype()){
-      Log.error("The flow from "+ node.getSource()+" to "+ node.getTarget()+" has no type but needs one.");
-    }
-
     var astNode = node.getEnclosingScope().getAstNode();
 
-    if(astNode instanceof ASTPartDef){
+    if(astNode instanceof ASTPartDef) {
 
     }
     //TODO check src and target exists
@@ -27,7 +20,5 @@ public class ConnectionGeneratorCoCos implements SysMLConnectionsASTFlowCoCo {
     //TODO directions are correct
 
   }
-
-
 
 }
