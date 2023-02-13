25e6bc922b8199b4282020f1558858f9093fe68b
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-02-13 16:21:09 +0100

    added getFlowOfPart to ComponentHelper

--- language/src/main/java/de/monticore/lang/sysmlv2/generator/ComponentHelper.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/generator/ComponentHelper.java
@@ -1,6 +1,9 @@
 package de.monticore.lang.sysmlv2.generator;
 
+import de.monticore.lang.sysmlbasis._ast.ASTSysMLElement;
+import de.monticore.lang.sysmlconnections._ast.ASTFlow;
 import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
+import de.monticore.lang.sysmlparts._ast.ASTPartDef;
 import de.monticore.lang.sysmlparts._ast.ASTPartUsage;
 import de.monticore.lang.sysmlparts._ast.ASTPortUsage;
 import de.monticore.lang.sysmlv2.types.SysMLBasisTypesFullPrettyPrinter;
@@ -8,10 +11,13 @@ import de.monticore.prettyprint.IndentPrinter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ComponentHelper {
   PartUtils partUtils = new PartUtils();
+
   ComponentUtils componentUtils = new ComponentUtils();
+
   GeneratorUtils generatorUtils = new GeneratorUtils();
 
   SysMLBasisTypesFullPrettyPrinter printer = new SysMLBasisTypesFullPrettyPrinter(new IndentPrinter());
@@ -30,12 +36,30 @@ public class ComponentHelper {
   }
 
   public boolean isObjectAttribute(ASTAttributeUsage astAttributeUsage) {
-    return !generatorUtils.getScalarValueMapping().containsValue(printer.prettyprint(generatorUtils.attributeType(astAttributeUsage)));
+    return !generatorUtils.getScalarValueMapping().containsValue(
+        printer.prettyprint(generatorUtils.attributeType(astAttributeUsage)));
   }
-  public boolean isPortDelayed(ASTPortUsage portUsage){
+
+  public boolean isPortDelayed(ASTPortUsage portUsage) {
     return componentUtils.isPortDelayed(portUsage);
   }
-  public String getValueTypeOfPort(ASTPortUsage portUsage){
+
+  public String getValueTypeOfPort(ASTPortUsage portUsage) {
     return componentUtils.getValueTypeOfPort(portUsage);
+  }
+
+  public List<ASTFlow> getFlowOfPart(ASTSysMLElement element) {
+    if(element instanceof ASTPartUsage) {
+      return ((ASTPartUsage) element).streamSysMLElements().filter(t -> t instanceof ASTFlow).map(
+          t -> (ASTFlow) t).collect(
+          Collectors.toList());
+    }
+    if(element instanceof ASTPartDef) {
+      return ((ASTPartDef) element).streamSysMLElements().filter(t -> t instanceof ASTFlow).map(
+          t -> (ASTFlow) t).collect(
+          Collectors.toList());
+    }
+
+    return new ArrayList<>();
   }
 }
