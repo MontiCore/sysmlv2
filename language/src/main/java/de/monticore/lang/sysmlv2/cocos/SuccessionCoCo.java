1d075340861cf005759eafccc1c85267ea003615
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-02-16 13:52:36 +0100

    reworked SuccessionCoCo

--- language/src/main/java/de/monticore/lang/sysmlv2/cocos/SuccessionCoCo.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/SuccessionCoCo.java
@@ -2,7 +2,6 @@ package de.monticore.lang.sysmlv2.cocos;
 
 import de.monticore.lang.sysmlactions._ast.ASTSysMLSuccession;
 import de.monticore.lang.sysmlactions._cocos.SysMLActionsASTSysMLSuccessionCoCo;
-import de.monticore.lang.sysmlstates._symboltable.ISysMLStatesScope;
 import de.monticore.lang.sysmlv2.types.SysMLBasisTypesFullPrettyPrinter;
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.types.mcbasictypes._ast.ASTMCType;
@@ -17,31 +16,26 @@ public class SuccessionCoCo implements SysMLActionsASTSysMLSuccessionCoCo {
   public void check(ASTSysMLSuccession node) {
     String srcName = "";
     boolean actionSrcPresent;
-    boolean stateSrcPresent = false;
     if(node.isPresentSrc()) {
       srcName = node.getSrc();
       actionSrcPresent = node.getEnclosingScope().resolveActionUsage(node.getSrc()).isPresent();
-      if(node.getEnclosingScope() instanceof ISysMLStatesScope) {
-        stateSrcPresent = ((ISysMLStatesScope) node.getEnclosingScope()).resolveStateUsage(node.getSrc()).isPresent();
-      }
+
       if(!node.getSrc().equals("start")) {
-        if(!actionSrcPresent && !stateSrcPresent) {
-          Log.error("Could not find Action or state usage with the name \"" + node.getSrc() + "\" for "
+        if(!actionSrcPresent) {
+          Log.error("Could not find action usage with the name \"" + node.getSrc() + "\" for "
               + srcName + ".");
         }
       }
+
     }
     else {
       Log.error("There is no src definition for the succession.");
     }
     boolean actionTgtPresent;
-    boolean stateTgtPresent = false;
     actionTgtPresent = node.getEnclosingScope().resolveActionUsage(node.getTgt()).isPresent();
-    if(node.getEnclosingScope() instanceof ISysMLStatesScope) {
-      stateTgtPresent = ((ISysMLStatesScope) node.getEnclosingScope()).resolveStateUsage(node.getTgt()).isPresent();
-    }
-    if(!actionTgtPresent && !stateTgtPresent) {
-      Log.error("Could not find Action or state usage with the name \"" + node.getTgt() + "\" for "
+
+    if(!actionTgtPresent) {
+      Log.error("Could not find action usage with the name \"" + node.getTgt() + "\" for "
           + srcName + ".");
     }
 
