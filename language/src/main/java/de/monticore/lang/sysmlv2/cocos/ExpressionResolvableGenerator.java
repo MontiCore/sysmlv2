a0647dd97f8c9a20911bcc314df7960f42e898c5
Robert Klingenberg <robert.klingenberg@rwth-aachen.de>
2023-03-02 10:21:51 +0100

    added ExpressionResolvableGenerator coco

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/ExpressionResolvableGenerator.java
@@ -0,0 +1,74 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.sysmlv2.cocos;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTNameExpression;
+import de.monticore.expressions.expressionsbasis._cocos.ExpressionsBasisASTNameExpressionCoCo;
+import de.monticore.lang.sysmlactions._ast.ASTActionDef;
+import de.monticore.lang.sysmlactions._ast.ASTActionUsage;
+import de.monticore.lang.sysmlbasis._ast.ASTSysMLElement;
+import de.monticore.lang.sysmlimportsandpackages._ast.ASTSysMLPackage;
+import de.monticore.lang.sysmlparts._ast.ASTPartDef;
+import de.monticore.lang.sysmlparts._ast.ASTPartUsage;
+import de.monticore.lang.sysmlstates._ast.ASTStateDef;
+import de.monticore.lang.sysmlstates._ast.ASTStateUsage;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.lang.sysmlv2._symboltable.SysMLv2Scope;
+import de.monticore.lang.sysmlv2.generator.utils.resolve.AttributeResolveUtils;
+import de.monticore.lang.sysmlv2.types.SysMLBasisTypesFullPrettyPrinter;
+import de.monticore.prettyprint.IndentPrinter;
+import de.monticore.types.mcbasictypes._ast.ASTMCType;
+import de.se_rwth.commons.logging.Log;
+
+public class ExpressionResolvableGenerator implements ExpressionsBasisASTNameExpressionCoCo {
+  AttributeResolveUtils attributeResolveUtils = new AttributeResolveUtils();
+
+  private String printName(ASTMCType type) {
+    return type.printType(new SysMLBasisTypesFullPrettyPrinter(new IndentPrinter()));
+  }
+
+  @Override
+  public void check(ASTNameExpression node) {
+    var scope = node.getEnclosingScope();
+    var parent = scope.getAstNode();
+
+    if(parent instanceof ASTPartUsage || parent instanceof ASTPartDef) {
+      if(!resolveInParts(node))
+        Log.error("Could not resolve attribute usage with the name \"" + node.getName() + "\" in a expression.");
+    }
+    if(parent instanceof ASTStateUsage || parent instanceof ASTStateDef) {
+      if(!resolveInBehaviour(node, (SysMLv2Scope) node.getEnclosingScope()))
+        Log.error("Could not resolve attribute usage with the name \"" + node.getName() + "\" in a expression.");
+    }
+    if(parent instanceof ASTActionUsage || parent instanceof ASTActionDef) {
+      if(!resolveInBehaviour(node, (SysMLv2Scope) node.getEnclosingScope()))
+        Log.error("Could not resolve attribute usage with the name \"" + node.getName() + "\" in a expression.");
+    }
+
+  }
+
+  boolean resolveInParts(ASTNameExpression node) {
+    var scope = (SysMLv2Scope) node.getEnclosingScope();
+    if(scope.resolveAttributeUsageDown(node.getName()).isPresent())
+      return true;
+    var attributesListPart = attributeResolveUtils.getAttributesOfElement((ASTSysMLElement) scope.getAstNode());
+    if(attributesListPart.stream().anyMatch(t -> t.getName().equals(node.getName())))
+      return true;
+    return false;
+  }
+
+  boolean resolveInBehaviour(ASTNameExpression node, SysMLv2Scope scope) {
+    var parent = scope.getAstNode();
+    if(parent instanceof ASTSysMLModel || parent instanceof ASTSysMLPackage)
+      return false;
+
+    if(scope.resolveAttributeUsageDown(node.getName()).isPresent())
+      return true;
+    var attributesListPart = attributeResolveUtils.getAttributesOfElement((ASTSysMLElement) parent);
+    if(attributesListPart.stream().anyMatch(t -> t.getName().equals(node.getName())))
+      return true;
+    if(parent instanceof ASTPartUsage || parent instanceof ASTPartDef) {
+      return resolveInParts(node);
+    }
+    return resolveInBehaviour(node, (SysMLv2Scope) parent.getEnclosingScope());
+  }
+}
