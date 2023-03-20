d70311dacad580dc672b3b69406950000d12337c
Mathias Pfeiffer <mpfeiffer@se-rwth.de>
2023-03-20 10:12:47 +0100

    #52 Cleanup of test resources

--- language/src/test/java/cocos/NameCompatible4IsabelleTest.java
+++ language/src/test/java/cocos/NameCompatible4IsabelleTest.java
@@ -21,19 +21,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NameCompatible4IsabelleTest {
   private final SysMLv2Parser parser = new SysMLv2Parser();
   private final SysMLv2Tool st = new SysMLv2Tool();
-
-  private final String validPath = "src/test/resources/cocos/NameCompatible4Isabelle/0_valid.sysml";
-  private final String invalidPath = "src/test/resources/cocos/NameCompatible4Isabelle/0_invalid.sysml";
 
   @BeforeAll
   public static void init(){
@@ -46,45 +46,62 @@ public class NameCompatible4IsabelleTest {
     Log.getFindings().clear();
   }
 
-  @Test
-  public void testValidName() throws IOException {
-    Optional<ASTSysMLModel> ast = parser.parse(validPath);
-    if(ast.isPresent()) {
-      st.createSymbolTable(ast.get());
-      SysMLv2CoCoChecker checker = new SysMLv2CoCoChecker();
-      checker.addCoCo((SysMLStatesASTStateDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTPartDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTPortDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLConstraintsASTConstraintDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLActionsASTActionDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLRequirementsASTRequirementDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLImportsAndPackagesASTSysMLPackageCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTAttributeDefCoCo) new NameCompatible4Isabelle());
-      checker.checkAll(ast.get());
-      assertTrue(Log.getFindings().isEmpty());
-    }else {
-      Assertions.fail("AST is not present");
-    }
+  @ParameterizedTest()
+  @ValueSource(strings = {
+    "state def a123{}",
+    "part def a222{}",
+    "port def a33{}",
+    "package aA4{}",
+    "attribute def aR5{}",
+    "action def aaaR{}",
+    "constraint def AB{true}",
+    "requirement def Ab{}"
+  })
+  public void testValidName(String model) throws IOException {
+    Optional<ASTSysMLModel> ast = parser.parse_String(model);
+    assertThat(ast).isPresent();
+
+    SysMLv2CoCoChecker checker = new SysMLv2CoCoChecker();
+    checker.addCoCo((SysMLStatesASTStateDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTPartDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTPortDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLConstraintsASTConstraintDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLActionsASTActionDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLRequirementsASTRequirementDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLImportsAndPackagesASTSysMLPackageCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTAttributeDefCoCo) new NameCompatible4Isabelle());
+    checker.checkAll(ast.get());
+    assertTrue(Log.getFindings().isEmpty());
   }
 
-  @Test
-  public void testInvalidName() throws IOException{
-    Optional<ASTSysMLModel> ast = parser.parse(invalidPath);
-    if(ast.isPresent()) {
-      st.createSymbolTable(ast.get());
-      SysMLv2CoCoChecker checker = new SysMLv2CoCoChecker();
-      checker.addCoCo((SysMLStatesASTStateDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTPartDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTPortDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLConstraintsASTConstraintDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLActionsASTActionDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLRequirementsASTRequirementDefCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLImportsAndPackagesASTSysMLPackageCoCo) new NameCompatible4Isabelle());
-      checker.addCoCo((SysMLPartsASTAttributeDefCoCo) new NameCompatible4Isabelle());
-      checker.checkAll(ast.get());
-      assertFalse(Log.getFindings().isEmpty());
-    }else {
-      Assertions.fail("AST is not present");
-    }
+  @ParameterizedTest()
+  @ValueSource(strings = {
+      "state def $ {}",
+      "part def $ {}",
+      "port def a$ {}",
+      "package _9$ {}",
+      "attribute def $$$ {}",
+      "action def $a$ {}",
+      "constraint def a$1 {true}",
+      "requirement def _$ {}"
+  })
+  public void testInvalidName(String model) throws IOException{
+    Optional<ASTSysMLModel> ast = parser.parse_String(model);
+    assertThat(ast).isPresent();
+
+    SysMLv2CoCoChecker checker = new SysMLv2CoCoChecker();
+    checker.addCoCo((SysMLStatesASTStateDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTPartDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTPortDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLConstraintsASTConstraintDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLActionsASTActionDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLRequirementsASTRequirementDefCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLImportsAndPackagesASTSysMLPackageCoCo) new NameCompatible4Isabelle());
+    checker.addCoCo((SysMLPartsASTAttributeDefCoCo) new NameCompatible4Isabelle());
+    checker.checkAll(ast.get());
+
+    assertFalse(Log.getFindings().isEmpty());
+    assertThat(Log.getFindings().stream().filter(it -> it.isError()).map(it -> it.getMsg()))
+        .contains("This name is not Isabelle compatible");
   }
 }
