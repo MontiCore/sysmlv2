6de8ae95b51639271f4b3e8d921c2d823fc62f8a
Emre Satan <emre.satan@rwth-aachen.de>
2023-05-25 10:42:05 +0200

    #110, #114, #117, #118 completed doubleInverter case study

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/StateDefCoCo.java
@@ -0,0 +1,20 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.sysmlv2.cocos;
+
+import de.monticore.lang.sysmlparts._ast.ASTPartDef;
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTPartDefCoCo;
+import de.monticore.lang.sysmlstates._ast.ASTStateUsage;
+import de.se_rwth.commons.logging.Log;
+
+import java.util.List;
+
+public class StateDefCoCo implements SysMLPartsASTPartDefCoCo {
+  @Override
+  public void check(ASTPartDef node) {
+    List<ASTStateUsage> stateDefs = node.getSysMLElements(ASTStateUsage.class);
+    if(stateDefs.size() != 1){
+      Log.warn("Part " + node.getName() + " must contain exactly one state usage!");
+    }
+  }
+}
+
