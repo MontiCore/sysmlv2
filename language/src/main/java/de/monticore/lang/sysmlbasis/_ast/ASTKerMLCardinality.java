cd22b3dd0bf2e7b00e4a88daac0861a5bcca5b53
Mathias Pfeiffer <mpfeiffer@se-rwth.de>
2023-12-14 16:30:27 +0100

    montibelle/frontend/transformer/sysmltransformer#98 Grammar Updates

---
+++ language/src/main/java/de/monticore/lang/sysmlbasis/_ast/ASTKerMLCardinality.java
@@ -0,0 +1,36 @@
+package de.monticore.lang.sysmlbasis._ast;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTLiteralExpression;
+import de.monticore.literals.mccommonliterals._ast.ASTNatLiteral;
+import de.se_rwth.commons.logging.Log;
+
+public class ASTKerMLCardinality extends ASTKerMLCardinalityTOP {
+
+  public int getLowerBound() {
+    if(getLower() instanceof ASTLiteralExpression) {
+      if(((ASTLiteralExpression) getLower()).getLiteral() instanceof ASTNatLiteral) {
+        ASTNatLiteral v = (ASTNatLiteral) ((ASTLiteralExpression) getLower()).getLiteral();
+        return v.getValue();
+      }
+    }
+    Log.error("0xKML001 KerML cardinality cannot produce a value for lower bound");
+    return -1;
+  }
+
+  public int getUpperBound() {
+    if(isPresentUpper()) {
+      if(getUpper() instanceof ASTLiteralExpression) {
+        if(((ASTLiteralExpression) getUpper()).getLiteral() instanceof ASTNatLiteral) {
+          ASTNatLiteral v = (ASTNatLiteral) ((ASTLiteralExpression) getUpper()).getLiteral();
+          return v.getValue();
+        }
+      }
+    }
+    else {
+      return getLowerBound();
+    }
+    Log.error("0xKML002 KerML cardinality cannot produce a value for upper bound");
+    return -1;
+  }
+
+}
---
+++ language/src/test/java/parser/DomainLibrariesTest.java
@@ -0,0 +1,215 @@
+package parser;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTNameExpression;
+import de.monticore.lang.sysmlexpressions._ast.ASTSysMLPower;
+import de.monticore.lang.sysmlparts._ast.ASTAttributeUsage;
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.lang.sysmlv2.SysMLv2Tool;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Diese Klasse sammelt alle Tests zu den SysML Domain Libraries (abgelegt unter resources).
+ * Ziel ist es die Grammatiken genau so weit aufzubohren, dass die Modelle parsen.
+ */
+public class DomainLibrariesTest {
+
+  static SysMLv2Tool tool;
+
+  @BeforeAll
+  public static void setup() {
+    tool = new SysMLv2Tool();
+  }
+
+  @BeforeEach
+  public void init() {
+    tool.init();
+    Log.init();
+  }
+
+  @Test
+  public void testIdentifiers() throws IOException {
+    long startTime = System.nanoTime();
+    tool = new SysMLv2Tool();
+    long newToolTime = System.nanoTime() - startTime;
+    startTime = System.nanoTime();
+    tool.init();
+    long initTime = System.nanoTime() - startTime;
+    startTime = System.nanoTime();
+    Log.init();
+    long logTime = System.nanoTime() - startTime;
+    var model = "attribute <g> gram;";
+    startTime = System.nanoTime();
+    var ast = SysMLv2Mill.parser().parse_String(model);
+    long parseTime = System.nanoTime() - startTime;
+    Log.info(""+newToolTime/1000000+"ms", "new Tool()");
+    Log.info(""+initTime/1000000+"ms", "tool.init()");
+    Log.info(""+logTime/1000000+"ms", "Log.init()");
+    Log.info(""+parseTime/1000000+"ms", "parse");
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testENotation() throws IOException {
+    var model = "1.0e-10";
+    var ast = SysMLv2Mill.parser().parse_StringScientificENotatationLiteral(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testSpecialCharacters() throws IOException {
+    var model = "attribute <S> siemens : ConductanceUnit = 'Ω'^-1;";
+    var ast = SysMLv2Mill.parser().parse_String(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+
+    var attr = (ASTAttributeUsage)ast.get().getSysMLElement(0);
+    var expr = ((ASTSysMLPower)attr.getExpression()).getExpression(0);
+    assertThat(expr).isInstanceOf(ASTNameExpression.class);
+    assertThat(((ASTNameExpression)expr).getName()).isEqualTo("Ω");
+  }
+
+  @Test
+  public void testName() throws IOException {
+    var model = "attribute <S> siemens : ConductanceUnit = Ohm^-1;";
+    var ast = SysMLv2Mill.parser().parse_String(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+
+    var attr = (ASTAttributeUsage)ast.get().getSysMLElement(0);
+    var expr = ((ASTSysMLPower)attr.getExpression()).getExpression(0);
+    assertThat(expr).isInstanceOf(ASTNameExpression.class);
+    assertThat(((ASTNameExpression)expr).getName()).isEqualTo("Ohm");
+  }
+
+  @Test
+  public void testKardinalität() throws IOException {
+    var model = "attribute temperatureDifference: TemperatureDifferenceValue [*] nonunique :> scalarQuantities;";
+    var ast = SysMLv2Mill.parser().parse_String(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testOrAndExpression() throws IOException {
+    var model = "assert constraint boundMatch { (isBound == mRef.isBound) or (not isBound and mRef.isBound) }";
+    var ast = SysMLv2Mill.parser().parse_String(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testCardinalities() throws IOException {
+    // Wenn n im Kontext existiert, sollte das eine valide Kardinalität sein
+    var model = "[1..n]";
+    var ast = SysMLv2Mill.parser().parse_StringSysMLCardinality(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testFunctionExpression() throws IOException {
+    var model = ""
+        + "(1..size(edges))->forAll {                                   \n"
+        + "  in i;                                                      \n"
+        + "  edges#(i).vertices->equals(                                \n"
+        + "    (vertices#((2*i)-1), vertices#(2*i))                     \n"
+        + "  )                                                          \n"
+        + "  and                                                        \n"
+        + "  includes(                                                  \n"
+        + "    (edges#(i).vertices#(2) as Item).matingOccurrences,      \n"
+        + "    edges#(if i==size(edges) ? 1 else i+1).vertices#(1)      \n"
+        + "  )                                                          \n"
+        + "}";
+    var ast = SysMLv2Mill.parser().parse_StringExpression(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testFunctionExpression1() throws IOException {
+    var model = ""
+        + "  includes(                                                  \n"
+        + "    (edges#(i).vertices#(2) as Item).matingOccurrences,      \n"
+        + "    edges#(if i==size(edges) ? 1 else i+1).vertices#(1)      \n"
+        + "  )";
+    //model = "(edges#(i).vertices#(2) as Item).matingOccurrences";
+    var ast = SysMLv2Mill.parser().parse_StringExpression(model);
+    assertThat(ast).isPresent();
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testParseShapeItems() {
+    var ast = tool.parse("src/main/resources/SysML Domain Libraries/Geometry/ShapeItems.sysml");
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testParse() {
+    var ast = tool.parse("src/main/resources/SysML Domain Libraries/Quantities and Units/SI.sysml");
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testParseISQ() {
+    var ast = tool.parse("src/main/resources/SysML Domain Libraries/Quantities and Units/ISQ.sysml");
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testParseQuantities() {
+    var ast = tool.parse("src/main/resources/SysML Domain Libraries/Quantities and Units/Quantities.sysml");
+    assertThat(Log.getFindings()).isEmpty();
+  }
+
+  @Test
+  public void testParseAll() throws IOException {
+    var models = Files.walk(Path.of("src/main/resources/SysML Domain Libraries"))
+        .filter(p -> p.toFile().getName().endsWith(".sysml"))
+        .collect(Collectors.toList());
+
+    assertThat(models).hasSize(37);
+    var successful = 0;
+    var lines = 0;
+
+    Log.enableFailQuick(false);
+    for(var model: models) {
+      try {
+        var ast = tool.parse(model.toString());
+        if(Log.getFindings().isEmpty()) {
+          successful++;
+        }
+      } catch (Exception e) {
+        // Erstmal nur messen
+      }
+      finally {
+        lines += Log.getFindingsCount();
+        Log.clearFindings();
+      }
+    }
+
+    System.out.println("Success rate: " + successful + "/" + 37 + " (" + lines + " findings)");
+  }
+
+  @Test
+  public void measureMillInit() {
+    var startTime = System.nanoTime();
+    SysMLv2Mill.init();
+    var elapsed = System.nanoTime() - startTime;
+    assertThat(elapsed).isLessThan(1*1000*1000*1000); // nano -> micro -> milli -> sekunden
+  }
+
+
+}
