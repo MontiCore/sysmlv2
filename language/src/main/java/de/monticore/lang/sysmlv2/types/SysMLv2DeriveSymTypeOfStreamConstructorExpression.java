f7162377ae494a85dec70befea00a7f6fc7b2e53
ling guo dei <guo.ling@rwth-aachen.de>
2025-05-15 01:47:02 +0200

    added repeat function for stream expressions and derived correct type for stream expression

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/types/SysMLv2DeriveSymTypeOfStreamConstructorExpression.java
@@ -0,0 +1,80 @@
+package de.monticore.lang.sysmlv2.types;
+
+import de.monticore.expressions.commonexpressions._visitor.CommonExpressionsTraverser;
+import de.monticore.expressions.expressionsbasis._visitor.ExpressionsBasisTraverser;
+import de.monticore.expressions.streamexpressions._ast.ASTStreamConstructorExpression;
+import de.monticore.expressions.streamexpressions._visitor.StreamExpressionsHandler;
+import de.monticore.expressions.streamexpressions._visitor.StreamExpressionsTraverser;
+import de.monticore.expressions.streamexpressions._visitor.StreamExpressionsVisitor2;
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.types.check.AbstractDeriveFromExpression;
+import de.monticore.types.check.SymTypeExpression;
+import de.monticore.types.check.SymTypeExpressionFactory;
+import de.se_rwth.commons.logging.Log;
+
+public class SysMLv2DeriveSymTypeOfStreamConstructorExpression extends
+    AbstractDeriveFromExpression implements
+    StreamExpressionsVisitor2, StreamExpressionsHandler{
+
+  protected StreamExpressionsTraverser traverser;
+  protected boolean isStream;
+
+  public SysMLv2DeriveSymTypeOfStreamConstructorExpression(boolean isStream) {
+    super();
+    this.isStream = isStream;
+  }
+
+  public StreamExpressionsTraverser getTraverser() {
+    return this.traverser;
+  }
+
+  @Override
+  public void setTraverser(StreamExpressionsTraverser streamExpressionsTraverser) {
+    this.traverser = streamExpressionsTraverser;
+  }
+
+  @Override
+  public void endVisit(ASTStreamConstructorExpression node) {
+    SymTypeExpression type = getTypeCheckResult().getResult();
+    calculateCorrectType(type);
+  }
+
+
+  protected void calculateCorrectType(SymTypeExpression type) {
+    if (this.isStream) {
+      //case for isStream == true
+      if (type.getTypeInfo().getName().contains("Stream")) {
+        //type is already Stream, set TypeCheckResult
+        getTypeCheckResult().setResult(type);
+      } else {
+        //but 'type' is not Stream, we create type of Stream based on 'type'
+        // resolve the globally defined generic Stream-type
+        var streamType = SysMLv2Mill.globalScope().resolveType("Stream");
+        if(streamType.isEmpty()) {
+          Log.error("Stream not defined in global scope. Initialize it with 'SysMLv2Mill.addStreamType()'!");
+        }
+        //type is like boolean, int...
+        //type = Stream<type>, create SymTypeOfGenerics Stream<type>
+        type = SymTypeExpressionFactory.createGenerics(streamType.get(), type);
+
+        //check whether type is a Stream
+        if (type.getTypeInfo().getName().contains("Stream")) {
+          getTypeCheckResult().setResult(type);
+        } else {
+          Log.error("0x81010 Type should be Stream");
+        }
+      }
+    } else {
+      //case for isStream == false
+      if (!type.getTypeInfo().getName().contains("Stream")) {
+        //type is also not Stream, set TypeCheckResult
+        getTypeCheckResult().setResult(type);
+      } else {
+        //but type is Stream
+        Log.error("0x81001 type should not be Stream");
+      }
+    }
+  }
+
+
+}
