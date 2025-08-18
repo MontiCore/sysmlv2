6ec09aacdbaa4fc67b3c0147737eb9ea963409dd
Alexander Hellwig <hellwig@se-rwth.de>
2025-08-18 09:51:55 +0200

    Fix language server compilation

--- language-server/src/main/java/de/monticore/lang/sysmlv2/_lsp/SysMLv2SyntaxHighlightingService.java
+++ language-server/src/main/java/de/monticore/lang/sysmlv2/_lsp/SysMLv2SyntaxHighlightingService.java
@@ -1,5 +1,6 @@
 package de.monticore.lang.sysmlv2._lsp;
 
+import de.mclsg.lsp.util.LanguageServerContext;
 import de.monticore.lang.sysmlv2._lsp.features.syntax_highlighting.SysMLv2LexerProvider;
 import de.monticore.lang.sysmlv2._lsp.language_access.SysMLv2LanguageAccess;
 import de.monticore.lang.sysmlv2._lsp.semantic_token.TimingClassification;
@@ -7,8 +8,8 @@ import de.monticore.lang.sysmlv2._lsp.semantic_token.TypeClassification;
 
 public class SysMLv2SyntaxHighlightingService extends SysMLv2SyntaxHighlightingServiceTOP {
 
-  public SysMLv2SyntaxHighlightingService(SysMLv2LanguageAccess languageAccess) {
-    super(languageAccess);
+  public SysMLv2SyntaxHighlightingService(SysMLv2LanguageAccess languageAccess, LanguageServerContext context) {
+    super(languageAccess, context);
   }
 
   protected void registerProviders(SysMLv2LanguageAccess languageAccess) {
