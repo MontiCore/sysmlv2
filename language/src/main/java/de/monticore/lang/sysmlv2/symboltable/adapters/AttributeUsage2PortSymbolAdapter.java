13bf1953b4c79e0a94c3ab2543b4aa67deaf15ce
Dovydas Skauranskas <109866625+DovydasSkauranskas@users.noreply.github.com>
2025-11-03 07:55:21 +0200

    import geloescht

--- language/src/main/java/de/monticore/lang/sysmlv2/symboltable/adapters/AttributeUsage2PortSymbolAdapter.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/symboltable/adapters/AttributeUsage2PortSymbolAdapter.java
@@ -4,7 +4,6 @@ import de.monticore.lang.componentconnector._symboltable.MildPortSymbol;
 import de.monticore.lang.sysmlparts._symboltable.AttributeUsageSymbol;
 import de.monticore.lang.sysmlparts._symboltable.PartDefSymbol;
 import de.monticore.lang.sysmlparts._symboltable.PortUsageSymbol;
-import de.monticore.lang.sysmlstates._symboltable.StateUsageSymbol;
 import de.monticore.lang.sysmlv2._symboltable.ISysMLv2Scope;
 import de.monticore.symbols.compsymbols._symboltable.ICompSymbolsScope;
 import de.monticore.symbols.compsymbols._symboltable.Timing;
