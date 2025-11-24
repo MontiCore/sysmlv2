4cc122c21e8f343a962615a3f2174e49107a6105
adriancostin-sd <marin@se-rwth.de>
2025-11-24 10:07:33 +0100

    typecheck conditional not

--- language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLTypeVisitorOperatorCalculator.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLTypeVisitorOperatorCalculator.java
@@ -1,10 +1,14 @@
 package de.monticore.lang.sysmlv2.types3;
 
+import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeExpressionFactory;
 import de.monticore.types3.SymTypeRelations;
+import de.monticore.types3.util.TypeVisitorLifting;
 import de.monticore.types3.util.TypeVisitorOperatorCalculator;
 import de.se_rwth.commons.logging.Log;
+
+import java.util.Optional;
 
 public class SysMLTypeVisitorOperatorCalculator extends
     TypeVisitorOperatorCalculator {
@@ -26,5 +30,21 @@ public class SysMLTypeVisitorOperatorCalculator extends
     }
 
     return SymTypeExpressionFactory.createObscureType();
+  }
+
+  public static Optional<SymTypeExpression> conditionalNot(SymTypeExpression inner) {
+    // TODO implement correctly
+    return ((SysMLTypeVisitorOperatorCalculator)getDelegate())._conditionalNot(inner);
+  }
+
+  protected Optional<SymTypeExpression> _conditionalNot(SymTypeExpression inner) {
+    SymTypeExpression result =
+        TypeVisitorLifting.liftDefault(this::calculateConditionalNot)
+            .apply(inner);
+    return obscure2Empty(result);
+  }
+
+  protected SymTypeExpression calculateConditionalNot(SymTypeExpression inner) {
+    return super.calculateLogicalNot(inner);
   }
 }
