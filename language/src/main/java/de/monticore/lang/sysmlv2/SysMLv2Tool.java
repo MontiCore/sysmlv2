bc3551a3dbaa65e6bb43673d16474a9b401397cf
adriancostin-sd <marin@se-rwth.de>
2025-12-06 21:04:22 +0100

    mill test measurements and tool

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/SysMLv2Tool.java
@@ -0,0 +1,27 @@
+package de.monticore.lang.sysmlv2;
+
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+
+import java.nio.file.Paths;
+
+public class SysMLv2Tool extends SysMLv2ToolTOP {
+  private long startTimeGlobal;
+  @Override
+  public void init() {
+    var startTime = System.nanoTime();
+    startTimeGlobal = startTime;
+    super.init();
+    var elapsed = System.nanoTime() - startTime;
+    System.out.println("Mill init: " + elapsed/1000/1000);
+  }
+
+  @Override
+  public ASTSysMLModel parse(String model) {
+    var startTime = System.nanoTime();
+    var ast = super.parse(model);
+    var elapsed = System.nanoTime() - startTime;
+    System.out.println(Paths.get(model).getFileName() + ": " + elapsed/1000/1000);
+    System.out.println("Total" + ": " + (System.nanoTime()/1000/1000 - startTimeGlobal/1000/1000));
+    return ast;
+  }
+}
--- language/src/test/java/MillTest.java
+++ language/src/test/java/MillTest.java
@@ -10,13 +10,18 @@ public class MillTest {
    * Es gab in einer früheren Version der Sprache Probleme mit der Mill: Die Initialsierung hat etwa 30 Sekunden
    * gedauert. Dieser Test prüft, dass die Initialsierungsdauer unter 1 Sekunde bleibt.
    */
-  @Disabled("Spoiler: Es dauert ~4,5 Sekunden auf meinem Laptop")
+  //@Disabled("Spoiler: Es dauert ~4,5 Sekunden auf meinem Laptop")
   @Test
   public void measureMillInit() {
-    var startTime = System.nanoTime();
-    SysMLv2Mill.init();
-    var elapsed = System.nanoTime() - startTime;
-    assertThat(elapsed).isLessThan(1*1000*1000*1000); // nano -> micro -> milli -> sekunden
+    for (int i = 1; i<=20; i++) {
+      var startTime = System.nanoTime();
+      SysMLv2Mill.init();
+      var elapsed = System.nanoTime() - startTime;
+      //assertThat(elapsed).isLessThan(
+      //    1 * 1000 * 1000 * 1000); // nano -> micro -> milli -> sekunden
+      System.out.println(i + ": micro: " + elapsed/1000 + " milli: " + elapsed/1000/1000);
+      SysMLv2Mill.reset();
+    }
   }
 
 }
