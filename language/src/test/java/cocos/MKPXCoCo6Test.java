0433498bb1c83e81f6d4a87b4e824be878f15105
MKZaito <127297267+MKZaito@users.noreply.github.com>
2025-12-09 18:39:19 +0100

    Test for MKPX_CoCo6

---
+++ language/src/test/java/cocos/MKPXCoCo6Test.java
@@ -0,0 +1,78 @@
+/* (c) https://github.com/MontiCore/monticore */
+package cocos;
+
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.lang.sysmlv2._cocos.SysMLv2CoCoChecker;
+import de.monticore.lang.sysmlv2.cocos.MKPX_CoCo6;
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTConnectionUsageCoCo;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MKPXCoCo6Test {
+
+  @BeforeAll
+  public static void init() {
+    Log.init();
+    SysMLv2Mill.init();
+  }
+
+  @BeforeEach
+  public void reset() {
+    SysMLv2Mill.globalScope().clear();
+    Log.getFindings().clear();
+    Log.enableFailQuick(true);
+  }
+
+  @Nested
+  public class InputConnectionTests {
+    @Test
+    public void testValid() throws IOException {
+      String validModel =
+          "part def A { port in: ~int; }"
+        + "part def B { port out: int; }"
+        + "part def System {"
+        +   "port sysIn: ~int;"
+        +   "port sysOut: int;"
+        +   "part a: A;"
+        +   "part b: B;"
+        +   "connect sysIn to a.in;"
+        +   "connect sysIn to sysOut;"
+        + "}";
+
+      ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(validModel).get();
+      SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+      var checker = new SysMLv2CoCoChecker();
+      checker.addCoCo((SysMLPartsASTConnectionUsageCoCo) new MKPX_CoCo6());
+      checker.checkAll(ast);
+      assertTrue(Log.getFindings().isEmpty());
+    }
+
+    @Test
+    public void testInvalid() throws IOException {
+      String invalidModel =
+          "part def A { port out: int; }"
+        + "part def System {"
+        +   "port sysIn: ~int;"
+        +   "part a: A;"
+        +   "connect sysIn to a.out;"
+        + "}";
+
+      ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(invalidModel).get();
+      SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+      var checker = new SysMLv2CoCoChecker();
+      checker.addCoCo((SysMLPartsASTConnectionUsageCoCo) new MKPX_CoCo6());
+      Log.enableFailQuick(false);
+      checker.checkAll(ast);
+      assertTrue(Log.getFindings().stream()
+              .anyMatch(f -> f.getMsg().contains("0xMKPX06")));
+    }
+  }
+}
