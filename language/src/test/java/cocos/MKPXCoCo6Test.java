830e4a2077e1f978a62019c21ce82af8f0d3cfb5
MKZaito <127297267+MKZaito@users.noreply.github.com>
2025-12-09 19:18:00 +0100

    Missing import assertFalse

--- language/src/test/java/cocos/MKPXCoCo6Test.java
+++ language/src/test/java/cocos/MKPXCoCo6Test.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MKPXCoCo6Test {
