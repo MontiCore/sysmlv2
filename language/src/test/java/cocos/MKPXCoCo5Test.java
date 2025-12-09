4302ef30f5ea8e4a357b52930210635d0c6b7c7b
MKZaito <127297267+MKZaito@users.noreply.github.com>
2025-12-09 19:00:57 +0100

    Missing Import SysMLv2Parser

--- language/src/test/java/cocos/MKPXCoCo5Test.java
+++ language/src/test/java/cocos/MKPXCoCo5Test.java
@@ -4,6 +4,7 @@ package cocos;
 import de.monticore.lang.sysmlv2.SysMLv2Mill;
 import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
 import de.monticore.lang.sysmlv2._cocos.SysMLv2CoCoChecker;
+import de.monticore.lang.sysmlv2._parser.SysMLv2Parser;
 import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTConnectionUsageCoCo;
 import de.monticore.lang.sysmlv2.cocos.MKPX_CoCo5;
 import de.se_rwth.commons.logging.Log;
