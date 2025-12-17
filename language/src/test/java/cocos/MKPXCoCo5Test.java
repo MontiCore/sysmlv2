5285bebcc0b656a2d6daa9b226dde71e719ffa2d
MKZaito <127297267+MKZaito@users.noreply.github.com>
2025-12-17 16:42:31 +0100

    Update MKPXCoCo5Test.java

--- language/src/test/java/cocos/MKPXCoCo5Test.java
+++ language/src/test/java/cocos/MKPXCoCo5Test.java
@@ -44,17 +44,19 @@ public class MKPXCoCo5Test {
     @Test
     public void testValid() throws IOException {
       String validModel =
-          "part def A { port output: int; }"
-        + "part def B { port input: ~int; }"
-        + "part def C { port output: int; }"
-        + "part def System {"
-        +   "port sysOutput: int;"
-        +   "part a: A;"
-        +   "part b: B;"
-        +   "part c: C;"
-        +   "connect a.output to b.input;"          // (Sub) Output -> (Sub) Input
-        +   "connect c.output to sysOutput;"        // (Sub) Output -> (main) Output
-        + "}";
+          "port def OutPort { out attribute data: int; }"
+              + "port def InPort { in attribute data: int; }"
+              + "part def A { port output: OutPort; }"
+              + "part def B { port input: InPort; }"
+              + "part def C { port output: OutPort; }"
+              + "part def System {"
+              +   "port sysOutput: OutPort;"
+              +   "part a: A;"
+              +   "part b: B;"
+              +   "part c: C;"
+              +   "connect a.output to b.input;"          // (Sub) Output -> (Sub) Input
+              +   "connect c.output to sysOutput;"        // (Sub) Output -> (main) Output
+              + "}";
       ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(validModel).get();
       SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
       var checker = new SysMLv2CoCoChecker();
@@ -66,13 +68,14 @@ public class MKPXCoCo5Test {
     @Test
     public void testInvalidSubOutToSubOut() throws IOException {
       String invalidModel =
-          "part def A { port output: int; }"
-        + "part def B { port output: int; }"
-        + "part def System {"
-        +   "part a: A;"
-        +   "part b: B;"
-        +   "connect a.output to b.output;"         // (Sub) Output -> (Sub) Output
-        + "}";
+          "port def OutPort { out attribute data: int; }"
+              + "part def A { port output: OutPort; }"
+              + "part def B { port output: OutPort; }"
+              + "part def System {"
+              +   "part a: A;"
+              +   "part b: B;"
+              +   "connect a.output to b.output;"         // (Sub) Output -> (Sub) Output
+              + "}";
 
       ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(invalidModel).get();
       SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
@@ -87,12 +90,14 @@ public class MKPXCoCo5Test {
     @Test
     public void testInvalidSubOutToMainIn() throws IOException {
       String invalidModel =
-          "part def A { port output: int; }"
-        + "part def System {"
-        +   "port sysInput: ~int;"
-        +   "part a: A;"
-        +   "connect a.output to sysInput;"         // (Sub) Output -> (main) Input
-        + "}";
+          "port def OutPort { out attribute data: int; }"
+              + "port def InPort { in attribute data: int; }"
+              + "part def A { port output: OutPort; }"
+              + "part def System {"
+              +   "port sysInput: InPort;"
+              +   "part a: A;"
+              +   "connect a.output to sysInput;"         // (Sub) Output -> (main) Input
+              + "}";
 
       ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(invalidModel).get();
       SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
