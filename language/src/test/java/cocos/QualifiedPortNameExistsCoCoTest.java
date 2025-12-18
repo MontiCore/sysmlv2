390516ccde5df7d0f280dd1277fa6b8e73494ed9
Mike <127297267+MKZaito@users.noreply.github.com>
2025-12-18 15:01:20 +0100

    QualifiedPortNameExistsCoCo (#35)
    * CoCo4
    
    * not responsible for undefined parts
    
    covered in MKPX_CoCo3
    
    * Test for MKPX_CoCo4
    
    * add parser and Model Path
    
    * Missing Import SysMLv2Parser
    
    * Missing import assertFalse
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * syntax error fixed
    
    * renamed and changed error code

---
+++ language/src/test/java/cocos/QualifiedPortNameExistsCoCoTest.java
@@ -0,0 +1,84 @@
+/* (c) https://github.com/MontiCore/monticore */
+package cocos;
+
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTConnectionUsageCoCo;
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTPartDefCoCo;
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTPartUsageCoCo;
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.lang.sysmlv2._cocos.SysMLv2CoCoChecker;
+import de.monticore.lang.sysmlv2._parser.SysMLv2Parser;
+import de.monticore.lang.sysmlv2.cocos.QualifiedPortNameExistsCoCo;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QualifiedPortNameExistsCoCoTest {
+
+  private static final String MODEL_PATH = "src/test/resources/parser";
+
+  private SysMLv2Parser parser = SysMLv2Mill.parser();
+
+  @BeforeAll
+  public static void init() {
+    Log.init();
+    SysMLv2Mill.init();
+  }
+
+  @BeforeEach
+  public void reset() {
+    SysMLv2Mill.globalScope().clear();
+    Log.getFindings().clear();
+    Log.enableFailQuick(true);
+  }
+
+  @Nested
+  public class QualifiedPortNameExistsCoCoTests {
+    @Test
+    public void testValid() throws IOException {
+      String validModel =
+          "part def A { port p: int; }"
+        + "part def B { port q: ~int; }"
+        + "part def System {"
+        +   "part a: A;"
+        +   "part b: B;"
+        +   "connect a.p to b.q;"
+        + "}";
+
+      ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(validModel).get();
+      SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+      var checker = new SysMLv2CoCoChecker();
+      checker.addCoCo(new QualifiedPortNameExistsCoCo());
+      checker.checkAll(ast);
+      assertTrue(Log.getFindings().isEmpty());
+    }
+
+    @Test
+    public void testInvalid() throws IOException {
+      String invalidModel =
+          "part def A { port p: int; }"
+        + "part def B { port q: ~int; }"
+        + "part def System {"
+        +   "part a: A;"
+        +   "part b: B;"
+        +   "connect a.wrongName to b.q;"
+        + "}";
+
+      ASTSysMLModel ast = SysMLv2Mill.parser().parse_String(invalidModel).get();
+      SysMLv2Mill.scopesGenitorDelegator().createFromAST(ast);
+      var checker = new SysMLv2CoCoChecker();
+      checker.addCoCo(new QualifiedPortNameExistsCoCo());
+      Log.enableFailQuick(false);
+      checker.checkAll(ast);
+      assertTrue(Log.getFindings().stream()
+              .anyMatch(f -> f.getMsg().contains("0x10AA4")));
+    }
+  }
+}
