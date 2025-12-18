5dcd4250ad66a5bfbf1703ef40fa07d12e0dbd3f
Mike <127297267+MKZaito@users.noreply.github.com>
2025-12-18 15:01:20 +0100

    QualifiedPortNameExistsCoCo (#35)
    * CoCo4
    
    * not responsible for undefined parts
    
    covered in MKPX_CoCo3
    
    * Test for MKPX_CoCo4
    
    * add parser and Model Path
    
    * Missing Import SysMLv2Parser
    
    * Missing import assertFalse
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * Update MKPXCoCo4Test.java
    
    * syntax error fixed
    
    * renamed and changed error code

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/cocos/QualifiedPortNameExistsCoCo.java
@@ -0,0 +1,96 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.sysmlv2.cocos;
+
+import de.monticore.lang.sysmlbasis._ast.ASTEndpoint;
+import de.monticore.lang.sysmlparts._ast.ASTConnectionUsage;
+import de.monticore.lang.sysmlparts._cocos.SysMLPartsASTConnectionUsageCoCo;
+import de.monticore.lang.sysmlparts._symboltable.ISysMLPartsScope;
+import de.monticore.lang.sysmlparts._symboltable.PartDefSymbol;
+import de.monticore.lang.sysmlparts._symboltable.PartUsageSymbol;
+import de.monticore.symboltable.modifiers.AccessModifier;
+import de.se_rwth.commons.logging.Log;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * QualifiedPortNameExistsCoCo:
+ * In einer Verbindung "connect a.b to c.d" muss jeder verwendete (qualifizierte) Portname existieren.
+ */
+public class QualifiedPortNameExistsCoCo implements SysMLPartsASTConnectionUsageCoCo {
+
+  @Override
+  public void check(ASTConnectionUsage node) {
+    if (!node.isPresentSrc() || !node.isPresentTgt()) {
+      return; // keine Verbindung
+    }
+
+    ISysMLPartsScope scope = node.getEnclosingScope();
+
+    checkEndpoint(scope, node.getSrc(), node);
+    checkEndpoint(scope, node.getTgt(), node);
+  }
+
+  protected void checkEndpoint(ISysMLPartsScope scope,
+                               ASTEndpoint endpoint,
+                               ASTConnectionUsage conn) {
+    String qname = endpointQName(endpoint);
+    if (qname.isEmpty()) {
+      return;
+    }
+
+    String[] parts = qname.split("\\.");
+
+    // Qualifizierter Name: a.b -> resolve a (PartUsage), dann b in PartDef
+    if (parts.length >= 2) {
+      String partName = parts[0];
+      String portName = parts[parts.length - 1];
+
+      Optional<PartUsageSymbol> partOpt = scope.resolvePartUsageLocally(partName);
+
+      if (partOpt.isEmpty()) {
+        // Existenz der Subkomponente wird bereits in MKPX_CoCo3 geprüft.
+        return;
+      }
+
+      var partDefOpt = partOpt.get().getPartDef();
+      if (partDefOpt.isEmpty()) {
+        // CoCo3
+        return;
+      }
+
+      PartDefSymbol partDef = partDefOpt.get();
+      boolean portExistsInDef = partDef.getSpannedScope()
+          .resolvePortUsageLocallyMany(false, portName, AccessModifier.ALL_INCLUSION, p -> true)
+          .size() == 1;
+
+      if (!portExistsInDef) {
+        Log.error(
+            "0x10AA4 The port '" + portName + "' does not exist in the definition of subcomponent '"
+                + partName + "'.",
+            conn.get_SourcePositionStart(),
+            conn.get_SourcePositionEnd()
+        );
+      }
+    }
+    else {
+      // Unqualifiziert: Port der Oberkomponente muss lokal existieren
+      String portName = parts[0];
+      List<?> localPorts = scope.resolvePortUsageLocallyMany(
+          false, portName, AccessModifier.ALL_INCLUSION, p -> true);
+      if (localPorts.isEmpty()) {
+        Log.error(
+            "0x10AA4 The port used in 'connect' '" + portName
+                + "' does not exist in the parent component.",
+            conn.get_SourcePositionStart(),
+            conn.get_SourcePositionEnd()
+        );
+      }
+    }
+  }
+
+  /** Liefert den qualifizierten Namen des Endpunkts als String. */
+  protected String endpointQName(ASTEndpoint ep) {
+    return ep.getMCQualifiedName() != null ? ep.getMCQualifiedName().toString() : "";
+  }
+}
