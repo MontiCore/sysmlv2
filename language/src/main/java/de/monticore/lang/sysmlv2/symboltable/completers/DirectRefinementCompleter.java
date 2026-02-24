30b322a5518c9c8eb3b36a6f21b33c35f22d146b
Mathias Pfeiffer <mpfeiffer@se-rwth.de>
2026-02-24 12:39:17 +0100

    7.8.17: COmplete refinements from Specialization too

--- language/src/main/java/de/monticore/lang/sysmlv2/symboltable/completers/DirectRefinementCompleter.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/symboltable/completers/DirectRefinementCompleter.java
@@ -1,6 +1,7 @@
 package de.monticore.lang.sysmlv2.symboltable.completers;
 
 import de.monticore.lang.sysmlbasis._ast.ASTSysMLRefinement;
+import de.monticore.lang.sysmlbasis._ast.ASTSysMLSpecialization;
 import de.monticore.lang.sysmlparts._ast.ASTPartDef;
 import de.monticore.lang.sysmlparts._visitor.SysMLPartsVisitor2;
 import de.monticore.lang.sysmlv2.SysMLv2Mill;
@@ -11,27 +12,53 @@ import de.monticore.types.check.TypeCheckResult;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+/**
+ * Adds specializations and refinements to the list of direct refinements.
+ * Example:
+ * "part def Specific specializes Abstract"
+ * Results in the "Specific"-PartDefSymbol to contain "Abstract" in the list of
+ * "directRefinements".
+ */
 public class DirectRefinementCompleter implements SysMLPartsVisitor2 {
 
   @Override
   public void visit(ASTPartDef node) {
+    var syn = new SysMLSynthesizer();
+
+    node.getSymbol().setDirectRefinementsList(
+        new ArrayList<SymTypeExpression>()
+    );
+
     var refinementTypes = node.getSpecializationList().stream()
         .filter(r -> r instanceof ASTSysMLRefinement)
         .flatMap(r -> r.getSuperTypesList().stream()).collect(Collectors.toList());
 
-    var syn = new SysMLSynthesizer();
-
-    var validRefinementExpressions = new ArrayList<SymTypeExpression>();
     for (var refinementType : refinementTypes) {
-      // Check existince of refinement before actually synthesizing it to avoid FATAL errors thrown by the synthesizer
+      // Check existence of refinement before actually synthesizing it to avoid
+      // FATAL errors thrown by the synthesizer
      if (refinementType.getDefiningSymbol().isEmpty()) {
        continue;
      }
       var result = syn.synthesizeType(refinementType);
       if (result.isPresentResult()) {
-        validRefinementExpressions.add(result.getResult());
+        node.getSymbol().addDirectRefinements(result.getResult());
       }
     }
-    node.getSymbol().setDirectRefinementsList(validRefinementExpressions);
+
+    var specializationTypes = node.getSpecializationList().stream()
+        .filter(r -> r instanceof ASTSysMLSpecialization)
+        .flatMap(r -> r.getSuperTypesList().stream()).collect(Collectors.toList());
+
+    for (var specializationType : specializationTypes) {
+      // Check existence of refinement before actually synthesizing it to avoid
+      // FATAL errors thrown by the synthesizer
+      if (specializationType.getDefiningSymbol().isEmpty()) {
+        continue;
+      }
+      var result = syn.synthesizeType(specializationType);
+      if (result.isPresentResult()) {
+        node.getSymbol().addDirectRefinements(result.getResult());
+      }
+    }
   }
 }
