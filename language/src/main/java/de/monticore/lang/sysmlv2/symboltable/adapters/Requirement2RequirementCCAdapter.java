a993b4920c7763b4095d944ccf4ca557ad9350c9
Justus R <103401898+justusrm@users.noreply.github.com>
2026-02-24 10:10:36 +0100

    7.8.15 Adapting CC-Requirement Adapter to work without sysml-ast reference (#65)
    * Removed intermediate constraints-ast from cc-ast representations
    
    * Version bump to 7.8.15

--- language/src/main/java/de/monticore/lang/sysmlv2/symboltable/adapters/Requirement2RequirementCCAdapter.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/symboltable/adapters/Requirement2RequirementCCAdapter.java
@@ -2,18 +2,12 @@
 package de.monticore.lang.sysmlv2.symboltable.adapters;
 
 import de.monticore.lang.componentconnector._symboltable.RequirementSymbol;
-import de.monticore.lang.sysmlconstraints._ast.ASTRequirementUsage;
 import de.monticore.lang.sysmlconstraints._symboltable.RequirementUsageSymbol;
 
 public class Requirement2RequirementCCAdapter extends RequirementSymbol  {
-  private ASTRequirementUsage ccAST;
 
   public Requirement2RequirementCCAdapter(RequirementUsageSymbol requirementUsageSymbol) {
     super(requirementUsageSymbol.getFullName());
-    this.ccAST = requirementUsageSymbol.getAstNode();
   }
 
-  public ASTRequirementUsage getCcAST() {
-    return ccAST;
-  }
 }
