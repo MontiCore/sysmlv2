93efa2a7efe4c6158c06fcdcff9ff967a4f89e8f
Justus Reusmann <justus@reusmann.de>
2026-03-31 21:25:14 +0200

    Forwardport to master: Extending CC-RequirementSymbol by invariant and subject fields

---
+++ cc/src/main/java/de/monticore/lang/componentconnector/_symboltable/RequirementSymbolDeSer.java
@@ -0,0 +1,45 @@
+package de.monticore.lang.componentconnector._symboltable;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.symboltable.serialization.json.JsonObject;
+
+import java.util.List;
+
+public class RequirementSymbolDeSer extends RequirementSymbolDeSerTOP {
+
+  @Override
+  protected void serializeSubject(MildComponentSymbol subject,
+                                  ComponentConnectorSymbols2Json s2j) {
+    // not implemented
+  }
+
+  @Override
+  protected void serializeAssumptions(List<ASTExpression> assumptions,
+                                      ComponentConnectorSymbols2Json s2j) {
+    // not implemented
+  }
+
+  @Override
+  protected void serializeGuarantee(ASTExpression guarantee,
+                                    ComponentConnectorSymbols2Json s2j) {
+    // not implemented
+  }
+
+  @Override
+  protected MildComponentSymbol deserializeSubject(JsonObject symbolJson) {
+    // not implemented
+    return null;
+  }
+
+  @Override
+  protected List<ASTExpression> deserializeAssumptions(JsonObject symbolJson) {
+    // not implemented
+    return List.of();
+  }
+
+  @Override
+  protected ASTExpression deserializeGuarantee(JsonObject symbolJson) {
+    // not implemented
+    return null;
+  }
+}
