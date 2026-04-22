9e140d980a3072ea5cc58c737556b9faee7d8b52
Justus R <justdevcode@gmail.com>
2026-04-22 14:49:05 +0200

    Forwardporting: Import resolving and tests for SysMLScopes

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/_symboltable/SysMLv2ScopesGenitor.java
@@ -0,0 +1,14 @@
+package de.monticore.lang.sysmlv2._symboltable;
+
+import de.monticore.lang.sysmlimportsandpackages._ast.ASTSysMLImportStatement;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.symboltable.ImportStatement;
+import de.se_rwth.commons.logging.Log;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class SysMLv2ScopesGenitor extends SysMLv2ScopesGenitorTOP {
+
+
+}
