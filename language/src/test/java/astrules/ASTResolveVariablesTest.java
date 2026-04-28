3429d042851afaf9397f41ddac3ee2520d7978cd
Marcel Grün <marcel.gruen1@rwth-aachen.de>
2026-04-28 03:57:29 +0200

    #251 Test zum resolven von Fields ohne Adapter erstellt

--- language/src/test/java/astrules/ASTResolveVariablesTest.java
+++ language/src/test/java/astrules/ASTResolveVariablesTest.java
@@ -1,37 +1,35 @@
 package astrules;
 
-import de.monticore.io.paths.MCPath;
-import de.monticore.lang.sysmlbasis._symboltable.ISysMLBasisScope;
 import de.monticore.lang.sysmlconstraints._ast.ASTConstraintUsage;
 import de.monticore.lang.sysmlimportsandpackages._ast.ASTSysMLPackage;
 import de.monticore.lang.sysmlv2.SysMLv2Mill;
 import de.monticore.lang.sysmlv2.SysMLv2Tool;
 
+import de.monticore.lang.sysmlv2._symboltable.ISysMLv2ArtifactScope;
 import de.monticore.lang.sysmlv2._symboltable.ISysMLv2Scope;
-import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
-import de.se_rwth.commons.logging.Log;
+import de.monticore.symbols.basicsymbols._symboltable.TypeSymbolBuilder;
+import de.monticore.symbols.basicsymbols._symboltable.VariableSymbolBuilder;
+import de.monticore.symbols.oosymbols._symboltable.FieldSymbolBuilder;
+import de.monticore.types.check.SymTypeExpressionFactory;
 import de.se_rwth.commons.logging.LogStub;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Diese Testklasse validiert die Resolve-Funktion für Variablenreferenzen in SysMLv2-Code.
+ * Diese Testklasse validiert die Resolve-Funktion für Feldausdrücke (wie 'a.b') in SysMLv2-Code.
  *
- * testCreateSymFiles erstellt symboltable-Dateien, um damit die Resolve-Funktion
- * zu testen ohne dass sie Zugriff auf den AST benötigt.
- *
- * testResolveOfVariables testet die Auflösung von Variablen wie 'a' und 'a.b' in einem Beispielmodell.
- *
+ * testResolveOfVariable testet die direkte Auflösung von Feldausdrücken wie 'a.b' durch Parsing eines
+ * kompletten SysMLv2-Modells. Das Ziel ist, einen Feldausdruck zu resolven, ohne vorher manuell die Variable
+ * zu resolven und dann in deren Typ nach dem Feld zu suchen. Weitere Tests mit komplexeren Verschachtelungen
+ * werden später hinzugefügt.
  */
 public class ASTResolveVariablesTest {
 
-  //@Disabled("Resolve von a.b schlägt noch fehl")
+  @Disabled("Resolve von a.b schlägt noch fehl")
   @Test
   public void testResolveOfVariable() throws IOException {
     LogStub.init();
@@ -55,5 +53,33 @@ public class ASTResolveVariablesTest {
 
     var variable = ((ISysMLv2Scope)expr.getEnclosingScope()).resolveField("a.b");
     assertThat(variable).isPresent();
+  }
+
+  @Disabled("Resolve von a.b schlägt noch fehl")
+  @Test
+  public void testResolveFieldWithoutAdapters() {
+    LogStub.init();
+    SysMLv2Mill.init();
+    ISysMLv2ArtifactScope artifactScope = SysMLv2Mill.artifactScope();
+    SysMLv2Mill.prepareGlobalScope();
+
+    var booleanSymbol = SysMLv2Mill.globalScope().resolveType("boolean").get();
+    artifactScope.add(booleanSymbol);
+
+    var boolType = SymTypeExpressionFactory.createPrimitive(booleanSymbol);
+    var fieldSymbol = new FieldSymbolBuilder().setName("b").setType(boolType).build();
+    artifactScope.add(fieldSymbol);
+    var typeAScope = SysMLv2Mill.scope();
+    typeAScope.add(fieldSymbol);
+    var typeSymbolA = new TypeSymbolBuilder().setName("A").setSpannedScope(typeAScope).build();
+    artifactScope.add(typeSymbolA);
+
+    var typeA = SymTypeExpressionFactory.createTypeObject(typeSymbolA);
+    var variableSymbol = new VariableSymbolBuilder().setName("a").setType(typeA).build();
+    artifactScope.add(variableSymbol);
+
+    var field = artifactScope.resolveField("a.b");
+    assertThat(field).isPresent();
+
   }
 }
