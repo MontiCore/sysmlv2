2c9da1e08b927a1391edc64fe2c10fa1f135b3c0
ling guo dei <guo.ling@rwth-aachen.de>
2026-05-19 22:15:58 +0200

    make final usable name and solve the possible ambiguity between protected modifier and userdefinedkeyword exposed by usage of nokeyword final

---
+++ language/src/test/java/parser/ParseFinalAsNameParserTest.java
@@ -0,0 +1,42 @@
+package parser;
+
+import de.monticore.lang.sysmlv2.SysMLv2Mill;
+import de.monticore.lang.sysmlv2._ast.ASTSysMLModel;
+import de.monticore.lang.sysmlv2._parser.SysMLv2Parser;
+import de.se_rwth.commons.logging.Log;
+import de.se_rwth.commons.logging.LogStub;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+  public class ParseFinalAsNameParserTest {
+    @BeforeAll
+    public static void init() {
+      LogStub.init();
+      Log.enableFailQuick(false);
+      SysMLv2Mill.init();
+    }
+
+    /**
+     *  Modifier "final" should be able to be used as
+     *  a name.
+     */
+    void testFinalAsStateName() throws IOException {
+      Log.clearFindings();
+      SysMLv2Parser parser = SysMLv2Mill.parser();
+
+      var model = "state final : MissionComplete;";
+
+      Optional<ASTSysMLModel> ast = parser.parse_String(model);
+
+      assertFalse(parser.hasErrors(), "Parsing should not have failed");
+      assertTrue(ast.isPresent(), "The AST should have been created");
+      assertTrue(Log.getFindings().isEmpty(), "No parser findings expected");
+    }
+  }
+
+
