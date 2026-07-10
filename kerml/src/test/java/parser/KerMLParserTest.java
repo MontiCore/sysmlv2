8be7e19f4213d898f976f4a9b83ec037c98e1e7a
hendrik.weskamp <hendrik.weskamp@rwth-aachen.de>
2026-07-11 01:15:35 +0200

    #348 parsed the KernelDataTypeLibrary kerml files

--- kerml/src/test/java/parser/KerMLParserTest.java
+++ kerml/src/test/java/parser/KerMLParserTest.java
@@ -3,8 +3,12 @@ package parser;
 import de.monticore.lang.kerml.KerMLMill;
 import de.monticore.lang.kerml._ast.ASTKerMLDocument;
 import de.monticore.lang.kerml._parser.KerMLParser;
+import de.se_rwth.commons.logging.Log;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -15,15 +19,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class KerMLParserTest {
 
   @BeforeAll
-  public static void init(){
+  public static void setUp(){
+    Log.init();
+    Log.enableFailQuick(false);
     KerMLMill.init();
   }
 
-  @Test
-  public void testParseTargetSentence() throws IOException {
-    String modelFile = "src/test/resources/parser/Collection.kerml";
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "Collections.kerml",
+      "ScalarValues.kerml",
+      "VectorValues.kerml"
+  })
+  public void testParseKernelDataTypeLibrary(String filename) throws IOException {
+    String modelFile = "src/test/resources/KernelDataTypeLibrary/" + filename;
     KerMLParser parser = KerMLMill.parser();
     Optional<ASTKerMLDocument> ast = parser.parse(modelFile);
+
 
     assertFalse(parser.hasErrors(), "Parser threw Errors!");
     assertTrue(ast.isPresent(), "Ast generation failed!");
