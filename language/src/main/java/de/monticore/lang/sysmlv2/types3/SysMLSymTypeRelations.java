ea710bae9eeaa898717aeab6cefc7bbe84f5ec56
MKZaito <127297267+MKZaito@users.noreply.github.com>
2026-07-20 12:12:10 +0200

    Kompatibilität von Booleans

--- language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLSymTypeRelations.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLSymTypeRelations.java
@@ -3,6 +3,7 @@ package de.monticore.lang.sysmlv2.types3;
 import de.monticore.ocl.types3.OCLSymTypeRelations;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types3.util.BuiltInTypeRelations;
+import de.monticore.types3.util.SymTypeCompatibilityCalculator;
 import de.monticore.types3.util.SymTypeRelationsDefaultDelegatee;
 
 public abstract class SysMLSymTypeRelations extends OCLSymTypeRelations {
@@ -21,6 +22,34 @@ public abstract class SysMLSymTypeRelations extends OCLSymTypeRelations {
           return super.isIntegralType(type) ||
               type.isPrimitive() &&
                   type.asPrimitive().getPrimitiveName().equals("nat");
+        }
+
+        /*
+        Any Type that is included here will be compatible with any other Type
+        in the list.
+         */
+        @Override
+        public boolean isBoolean(SymTypeExpression type) {
+          return (super.isBoolean(type) ||
+            (
+              type.hasTypeInfo() &&
+              type.getTypeInfo().getFullName().equals("ScalarValues.Boolean")
+            )
+          );
+        }
+      };
+
+      compatibilityDelegate = new SymTypeCompatibilityCalculator() {
+        @Override
+        public boolean isCompatible(
+            SymTypeExpression target,
+            SymTypeExpression source) {
+          return (super.isCompatible(target, source) ||
+            (
+              builtInRelationsDelegate.isBoolean(target) &&
+              builtInRelationsDelegate.isBoolean(source)
+            )
+          );
         }
       };
     }
