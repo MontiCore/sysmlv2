9e51f555f80cdf7c60b0b8a31222710935bd2ccf
Mathias Pfeiffer <mpfeiffer@se-rwth.de>
2026-08-03 14:01:17 +0200

    TODOs für #369

---
+++ kerml/src/test/java/parser/ParserTest.java
@@ -0,0 +1,24 @@
+package parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ParserTest {
+
+  /**
+   * Dieser Test ex. weil...
+   * TODO
+   */
+  @Test
+  public void testSonderfall() {
+    LogStub.init();
+    var tool = new KerMLTool();
+    tool.init();
+
+    // TODO Was war der Sonderfall, der zur Binding Strength "130" geführt hat:
+    var ast = KerMLMill.getParser().parse_StringExpression("a#b").get();
+
+    assertTrue(ast instanceof KerMLIndexExpression);
+  }
+}
