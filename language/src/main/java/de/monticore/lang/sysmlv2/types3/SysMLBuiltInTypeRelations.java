8325b4ef2dcd04b71ddaa6e8858cd8e458d5f8b2
MKZaito <127297267+MKZaito@users.noreply.github.com>
2026-08-31 16:02:09 +0200

    alles inlinen

--- language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLBuiltInTypeRelations.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLBuiltInTypeRelations.java
@@ -2,32 +2,30 @@ package de.monticore.lang.sysmlv2.types3;
 
 import de.monticore.types.check.SymTypeExpression;
 
-
+/**
+ * Dient dazu die MontiCore Built-Ins (int, String, boolean, nat) mit den
+ * KerML-ScalarValues (Integer, String, Boolean, Natural/Positive) kompatibel
+ * zu machen.
+ */
 public class SysMLBuiltInTypeRelations extends de.monticore.types3.util.BuiltInTypeRelations{
 
   @Override
   public boolean isIntegralType(SymTypeExpression type) {
-    return super.isIntegralType(type) ||
-      isNat(type) ||
-      checkFullName(type, "ScalarValues.Integer");
+    return super.isIntegralType(type)
+        || (type.isPrimitive()
+          && type.asPrimitive().getPrimitiveName().equals("nat"))
+        || (type.hasTypeInfo()
+          && (
+            type.getTypeInfo().getFullName().equals("ScalarValues.Integer") ||
+            type.getTypeInfo().getFullName().equals("ScalarValues.Natural") ||
+            type.getTypeInfo().getFullName().equals("ScalarValues.Positive"))
+    );
   }
 
   @Override
   public boolean isBoolean(SymTypeExpression type) {
-    return super.isBoolean(type) || checkFullName(type, "ScalarValues.Boolean");
-  }
-
-  public boolean isNat(SymTypeExpression type) {
-    return (
-      type.isPrimitive() &&
-      type.asPrimitive().getPrimitiveName().equals("nat")
-    ) ||
-      checkFullName(type, "ScalarValues.Natural") ||
-      checkFullName(type, "ScalarValues.Positive");
-  }
-
-  private boolean checkFullName(SymTypeExpression type, String fullName) {
-    return type.hasTypeInfo() &&
-      type.getTypeInfo().getFullName().equals(fullName);
+    return super.isBoolean(type) ||
+      (type.hasTypeInfo() &&
+        type.getTypeInfo().getFullName().equals("ScalarValues.Boolean"));
   }
 }
