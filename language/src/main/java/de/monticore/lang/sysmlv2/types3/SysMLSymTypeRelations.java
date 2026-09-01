e91277369d2b707b33b3238a4d8b59f2353c803b
MKZaito <127297267+MKZaito@users.noreply.github.com>
2026-09-01 15:29:08 +0200

    use SysMLSymTypeBoxingVisitor

--- language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLSymTypeRelations.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLSymTypeRelations.java
@@ -1,6 +1,7 @@
 package de.monticore.lang.sysmlv2.types3;
 
 import de.monticore.lang.sysmlv2.types3.util.SysMLSymTypeBoxingVisitor;
+import de.monticore.lang.sysmlv2.types3.util.SysMLSymTypeCompatibilityCalculator;
 import de.monticore.ocl.types3.OCLSymTypeRelations;
 import de.monticore.types3.util.SymTypeRelationsDefaultDelegatee;
 
@@ -16,6 +17,7 @@ public abstract class SysMLSymTypeRelations extends OCLSymTypeRelations {
     public SysMLSymTypeRelationsDelegatee() {
       builtInRelationsDelegate = new SysMLBuiltInTypeRelations();
       boxingVisitor = new SysMLSymTypeBoxingVisitor();
+      compatibilityDelegate = new SysMLSymTypeCompatibilityCalculator();
     }
   }
 }
