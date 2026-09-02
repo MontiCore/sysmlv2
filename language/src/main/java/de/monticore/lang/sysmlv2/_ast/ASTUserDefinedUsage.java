603f0c41c2405bdfcebef5776aafe558849406f1
Martin Maisak <maisak68@gmail.com>
2026-09-02 10:50:54 +0200

    Kommentar hinzugefügt

--- language/src/main/java/de/monticore/lang/sysmlv2/_ast/ASTUserDefinedUsage.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/_ast/ASTUserDefinedUsage.java
@@ -4,6 +4,14 @@ import de.se_rwth.commons.logging.Log;
 
 public class ASTUserDefinedUsage extends ASTUserDefinedUsageTOP {
 
+  /**
+   * UserDefinedUsage implementiert ASTSysMLUsage, das die Methode setModifier mit
+   * dem allgemeinen UML-Modifier-Typ deklariert. Die generierte TOP-Klasse
+   * speichert jedoch den spezifischeren SysMLv2-Modifier-Typ. Dieser Override ist
+   * daher nötig, um die Interface-Methode zu implementieren und kompatible
+   * SysMLv2-Modifier an den generierten Setter weiterzuleiten.
+   */
+
   @Override
   public void setModifier(de.monticore.umlmodifier._ast.ASTModifier modifier) {
     if (modifier == null || modifier instanceof ASTModifier) {
