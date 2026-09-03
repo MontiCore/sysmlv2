d4fdf01bbc7b931f0ab7bee22ed6bccb856ba5c0
Mike <127297267+MKZaito@users.noreply.github.com>
2026-09-03 16:09:00 +0200

    7.8.86: numerische Typen aus ScalarValues werden als solche erkannt (#219)
    * isNumericType
    
    * isDouble
    
    * isInt
    
    * isString
    
    * simplify isNumericType
    
    * expand test
    
    * ScalarValues.Positive untermenge von Natural
    
    * version bumb to 7.8.86

--- language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLBuiltInTypeRelations.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/SysMLBuiltInTypeRelations.java
@@ -10,6 +10,11 @@ import de.monticore.types.check.SymTypeExpression;
 public class SysMLBuiltInTypeRelations extends de.monticore.types3.util.BuiltInTypeRelations{
 
   @Override
+  public boolean isNumericType(SymTypeExpression type) {
+    return super.isNumericType(type) || isIntegralType(type) || isDouble(type);
+  }
+
+  @Override
   public boolean isIntegralType(SymTypeExpression type) {
     return super.isIntegralType(type)
         || (type.isPrimitive()
@@ -27,5 +32,35 @@ public class SysMLBuiltInTypeRelations extends de.monticore.types3.util.BuiltInT
     return super.isBoolean(type) ||
       (type.hasTypeInfo() &&
         type.getTypeInfo().getFullName().equals("ScalarValues.Boolean"));
+  }
+
+  @Override
+  public boolean isDouble(SymTypeExpression type) {
+    return super.isDouble(type)
+      || (type.hasTypeInfo() && (
+        type.getTypeInfo().getFullName().equals("ScalarValues.Real") ||
+        type.getTypeInfo().getFullName().equals("ScalarValues.Rational"))
+    );
+  }
+
+  @Override
+  public boolean isInt(SymTypeExpression type) {
+    return super.isInt(type)
+      || (type.isPrimitive()
+        && type.asPrimitive().getPrimitiveName().equals("nat"))
+      || (type.hasTypeInfo() && (
+        type.getTypeInfo().getFullName().equals("ScalarValues.Natural") ||
+        type.getTypeInfo().getFullName().equals("ScalarValues.Positive"))
+    );
+  }
+
+  @Override
+  public boolean isString(SymTypeExpression type) {
+    boolean isScalarValuesString = false;
+    if (type.isObjectType()) {
+      isScalarValuesString = type.hasTypeInfo()
+        && (type.getTypeInfo().getFullName().equals("ScalarValues.String"));
+    }
+    return super.isString(type) || isScalarValuesString;
   }
 }
