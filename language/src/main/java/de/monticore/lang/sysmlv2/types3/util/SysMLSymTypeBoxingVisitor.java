00c7baff2603eb2b975307d75559dc95c5b01a58
MKZaito <127297267+MKZaito@users.noreply.github.com>
2026-09-07 09:50:53 +0200

    SysMLSymTypeBoxingVisitor extend boxing map

---
+++ language/src/main/java/de/monticore/lang/sysmlv2/types3/util/SysMLSymTypeBoxingVisitor.java
@@ -0,0 +1,30 @@
+package de.monticore.lang.sysmlv2.types3.util;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SysMLSymTypeBoxingVisitor extends de.monticore.types3.util.SymTypeBoxingVisitor {
+
+  protected static final Map<String, String> sysMLPrimitiveBoxMap;
+  protected static final Map<String, String> sysMLObjectBoxMap;
+
+  static {
+    Map<String, String> primitiveBoxMap_temp = new HashMap<>(primitiveBoxMap);
+    primitiveBoxMap_temp.put("nat", "ScalarValues.Natural");
+    sysMLPrimitiveBoxMap = Collections.unmodifiableMap(primitiveBoxMap_temp);
+
+    Map<String, String> objectBoxMap_temp = new HashMap<>(objectBoxMap);
+    sysMLObjectBoxMap = Collections.unmodifiableMap(objectBoxMap_temp);
+  }
+
+  @Override
+  public Map<String, String> getPrimitiveBoxMap() {
+    return sysMLPrimitiveBoxMap;
+  }
+
+  @Override
+  public Map<String, String> getObjectBoxMap() {
+    return sysMLObjectBoxMap;
+  }
+}
