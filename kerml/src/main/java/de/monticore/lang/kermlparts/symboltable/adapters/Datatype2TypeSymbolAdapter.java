d68eb593e9dc3e73fe30a09d448d99a5da65ab62
hendrik.weskamp <hendrik.weskamp@rwth-aachen.de>
2026-09-12 07:38:18 +0200

    #399 removed comment

--- kerml/src/main/java/de/monticore/lang/kermlparts/symboltable/adapters/Datatype2TypeSymbolAdapter.java
+++ kerml/src/main/java/de/monticore/lang/kermlparts/symboltable/adapters/Datatype2TypeSymbolAdapter.java
@@ -3,10 +3,7 @@ package de.monticore.lang.kermlparts.symboltable.adapters;
 import de.monticore.lang.kermlelements._symboltable.DatatypeSymbolDeSer;
 import de.monticore.symbols.basicsymbols._symboltable.TypeSymbol;
 
-/**
- * Serializes KerML datatypes with the compatible {@link TypeSymbol} kind,
- * retaining the fields and nested scopes written by the KerML serializer.
- */
+
 public class Datatype2TypeSymbolAdapter extends DatatypeSymbolDeSer {
 
   @Override
