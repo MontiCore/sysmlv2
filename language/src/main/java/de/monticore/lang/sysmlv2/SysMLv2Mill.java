c30327e717bee727fe3db5a6593754d6d8dbbfe7
hendrik.weskamp <hendrik.weskamp@rwth-aachen.de>
2026-09-22 18:46:47 +0200

    #432 another typo

--- language/src/main/java/de/monticore/lang/sysmlv2/SysMLv2Mill.java
+++ language/src/main/java/de/monticore/lang/sysmlv2/SysMLv2Mill.java
@@ -39,7 +39,7 @@ public class SysMLv2Mill extends SysMLv2MillTOP {
     SysMLv2Mill.addStringType();
     SysMLv2Mill.loadScalarValuesFromSym();
     SysMLv2Mill.addScalarFunctionsTypes();
-    SysMLv2Mill.addKermlCollecitonsTypes();
+    SysMLv2Mill.addKermlCollectionsTypes();
     //SysMLv2Mill.loadCollectionValuesFromSym();
     SysMLv2Mill.addVectorValuesTypes();
     SysMLv2Mill.addCollectionTypes();
